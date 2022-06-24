435d377ee0a6c470615755c7271d9367a2015752
shailu-s <emailid462@gmail.com>
2022-06-24 18:26:49 +0530

    VOL-952: Updated library folder name to libs

---
+++ contracts/libs/Funding.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.7.6;
+
+import { PerpMath } from "./PerpMath.sol";
+import { OpenOrder } from "./OpenOrder.sol";
+import { PerpSafeCast } from "./PerpSafeCast.sol";
+import { PerpFixedPoint96 } from "./PerpFixedPoint96.sol";
+import { SignedSafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SignedSafeMathUpgradeable.sol";
+
+library Funding {
+    using PerpSafeCast for uint256;
+    using PerpSafeCast for uint128;
+    using SignedSafeMathUpgradeable for int256;
+
+    //
+    // STRUCT
+    //
+
+    /// @dev tw: time-weighted
+    /// @param twPremiumX96 overflow inspection (as twPremiumX96 > twPremiumDivBySqrtPriceX96):
+    //         max = 2 ^ (255 - 96) = 2 ^ 159 = 7.307508187E47
+    //         assume premium = 10000, time = 10 year = 60 * 60 * 24 * 365 * 10 -> twPremium = 3.1536E12
+    struct Growth {
+        int256 twPremiumX96;
+        int256 twPremiumDivBySqrtPriceX96;
+    }
+
+    //
+    // CONSTANT
+    //
+
+    /// @dev block-based funding is calculated as: premium * timeFraction / 1 day, for 1 day as the default period
+    int256 internal constant _DEFAULT_FUNDING_PERIOD = 1 days;
+
+    //
+    // INTERNAL PURE
+    //
+
+    function calcPendingFundingPaymentWithLiquidityCoefficient(
+        int256 baseBalance,
+        int256 twPremiumGrowthGlobalX96,
+        Growth memory fundingGrowthGlobal,
+        int256 liquidityCoefficientInFundingPayment
+    ) internal pure returns (int256) {
+        int256 balanceCoefficientInFundingPayment =
+            PerpMath.mulDiv(
+                baseBalance,
+                fundingGrowthGlobal.twPremiumX96.sub(twPremiumGrowthGlobalX96),
+                uint256(PerpFixedPoint96._IQ96)
+            );
+
+        return
+            liquidityCoefficientInFundingPayment.add(balanceCoefficientInFundingPayment).div(_DEFAULT_FUNDING_PERIOD);
+    }
+}
---
+++ contracts/libs/OpenOrder.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.7.6;
+
+library OpenOrder {
+    /// @param lastFeeGrowthInsideX128 fees in quote token recorded in Exchange
+    ///        because of block-based funding, quote-only and customized fee, all fees are in quote token
+    struct Info {
+        uint128 liquidity;
+        int24 lowerTick;
+        int24 upperTick;
+        uint256 lastFeeGrowthInsideX128;
+        int256 lastTwPremiumGrowthInsideX96;
+        int256 lastTwPremiumGrowthBelowX96;
+        int256 lastTwPremiumDivBySqrtPriceGrowthInsideX96;
+        uint256 baseDebt;
+        uint256 quoteDebt;
+    }
+
+    struct AssetType {
+        bytes4 assetClass;
+        bytes data;
+    }
+
+    struct Asset {
+        AssetType assetType;
+        uint256 value;
+    }
+
+    struct Order {
+        address maker;
+        Asset makeAsset;
+        address taker;
+        Asset takeAsset;
+        uint256 minTakeValue;
+        uint256 salt; // needs to be incremental for a given user
+        uint256 start;
+        uint256 end;
+        bytes data;
+    }
+
+    function calcOrderKey(
+        address trader,
+        address baseToken,
+        int24 lowerTick,
+        int24 upperTick
+    ) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(trader, baseToken, lowerTick, upperTick));
+    }
+}
