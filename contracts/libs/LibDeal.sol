7295c9a7d4f21ed1e93b26cead1a1c12338c64ee
shailu-s <emailid462@gmail.com>
2022-06-28 19:10:25 +0530

    VOL-958: Removed part lib from deal

--- contracts/libs/LibDeal.sol
+++ contracts/libs/LibDeal.sol
@@ -3,14 +3,11 @@ pragma solidity 0.7.6;
 pragma abicoder v2;
 
 import "./LibAsset.sol";
-import "./LibPart.sol";
 import "./LibFeeSide.sol";
 
 library LibDeal {
     struct DealSide {
         LibAsset.Asset asset;
-        LibPart.Part[] payouts;
-        LibPart.Part[] originFees;
         address proxy;
         address from;
     }
