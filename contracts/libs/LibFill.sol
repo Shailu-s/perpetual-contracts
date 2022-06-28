65f576ea9042f949843681e90b50a2f3464e38e1
shailu-s <emailid462@gmail.com>
2022-06-28 19:10:01 +0530

    VOL-958: Removed right annd left make fill bool

--- contracts/libs/LibFill.sol
+++ contracts/libs/LibFill.sol
@@ -25,21 +25,15 @@ library LibFill {
      * @param rightOrder right order
      * @param leftOrderFill current fill of the left order (0 if order is unfilled)
      * @param rightOrderFill current fill of the right order (0 if order is unfilled)
-     * @param leftIsMakeFill true if left orders fill is calculated from the make side, false if from the take side
-     * @param rightIsMakeFill true if right orders fill is calculated from the make side, false if from the take side
      */
     function fillOrder(
         LibOrder.Order memory leftOrder,
         LibOrder.Order memory rightOrder,
         uint256 leftOrderFill,
-        uint256 rightOrderFill,
-        bool leftIsMakeFill,
-        bool rightIsMakeFill
+        uint256 rightOrderFill
     ) internal pure returns (FillResult memory) {
-        (uint256 leftMakeValue, uint256 leftTakeValue) =
-            LibOrder.calculateRemaining(leftOrder, leftOrderFill, leftIsMakeFill);
-        (uint256 rightMakeValue, uint256 rightTakeValue) =
-            LibOrder.calculateRemaining(rightOrder, rightOrderFill, rightIsMakeFill);
+        (uint256 leftMakeValue, uint256 leftTakeValue) = LibOrder.calculateRemaining(leftOrder, leftOrderFill);
+        (uint256 rightMakeValue, uint256 rightTakeValue) = LibOrder.calculateRemaining(rightOrder, rightOrderFill);
 
         //We have 3 cases here:
         if (rightTakeValue > leftMakeValue) {
