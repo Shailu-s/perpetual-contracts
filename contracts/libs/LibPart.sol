fcc3bc5932bd70a9c02fd21f85c23c8da616105a
shailu-s <emailid462@gmail.com>
2022-06-28 14:32:13 +0530

    VOL-955: Added part lib

---
+++ contracts/libs/LibPart.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL - 1.1
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+library LibPart {
+    bytes32 public constant TYPE_HASH = keccak256("Part(address account,uint96 value)");
+
+    struct Part {
+        address payable account;
+        uint96 value;
+    }
+
+    function hash(Part memory part) internal pure returns (bytes32) {
+        return keccak256(abi.encode(TYPE_HASH, part.account, part.value));
+    }
+}
