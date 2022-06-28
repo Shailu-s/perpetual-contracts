8d6ab64555c4b426f9b88896ec4e11685fe2d2ba
shailu-s <emailid462@gmail.com>
2022-06-28 16:01:58 +0530

    VOL-955: Updated match asset signature in interface

--- contracts/interfaces/IAssetMatcher.sol
+++ contracts/interfaces/IAssetMatcher.sol
@@ -3,11 +3,11 @@
 pragma solidity 0.7.6;
 pragma abicoder v2;
 
-import "../libs/ExchangeFee.sol";
+import "../libs/LibAsset.sol";
 
 interface IAssetMatcher {
-    function matchAssets(
-        LibAsset.AssetType memory leftAssetType,
-        LibAsset.AssetType memory rightAssetType
-    ) external view returns (LibAsset.AssetType memory);
+    function matchAssets(LibAsset.Asset memory leftAsset, LibAsset.Asset memory rightAsset)
+        external
+        pure
+        returns (LibAsset.Asset memory);
 }
