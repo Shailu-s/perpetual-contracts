4fb91d7790314298e489aabd59673416c5802d53
shailu-s <emailid462@gmail.com>
2022-06-28 16:20:21 +0530

    VOL-955: Removed fee side logic for erc20

--- contracts/libs/LibFeeSide.sol
+++ contracts/libs/LibFeeSide.sol
@@ -5,15 +5,9 @@ pragma abicoder v2;
 import "./LibAsset.sol";
 
 library LibFeeSide {
-    enum FeeSide { NONE, LEFT, RIGHT }
+    enum FeeSide { LEFT }
 
-    function getFeeSide(bytes4 leftClass, bytes4 rightClass) internal pure returns (FeeSide) {
-        if (leftClass == LibAsset.ERC20_ASSET_CLASS) {
-            return FeeSide.LEFT;
-        }
-        if (rightClass == LibAsset.ERC20_ASSET_CLASS) {
-            return FeeSide.RIGHT;
-        }
-        return FeeSide.NONE;
+    function getFeeSide() internal pure returns (FeeSide) {
+        return FeeSide.LEFT;
     }
 }
