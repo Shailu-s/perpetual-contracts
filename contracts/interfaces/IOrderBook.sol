821ef730fdd1817dd076e261eec0f2dd14e6f02f
shailu-s <emailid462@gmail.com>
2022-06-29 14:51:25 +0530

    VOL-930: Updated positioning interface

--- contracts/interfaces/IOrderBook.sol
+++ contracts/interfaces/IOrderBook.sol
@@ -2,60 +2,9 @@
 pragma solidity 0.7.6;
 pragma abicoder v2;
 
-import { Funding } from "../libs/Funding.sol";
 import { OpenOrder } from "../libs/OpenOrder.sol";
 
 interface IOrderBook {
-    struct AddLiquidityParams {
-        address trader;
-        address baseToken;
-        uint256 base;
-        uint256 quote;
-        int24 lowerTick;
-        int24 upperTick;
-        Funding.Growth fundingGrowthGlobal;
-    }
-
-    struct RemoveLiquidityParams {
-        address maker;
-        address baseToken;
-        int24 lowerTick;
-        int24 upperTick;
-        uint128 liquidity;
-    }
-
-    struct AddLiquidityResponse {
-        uint256 base;
-        uint256 quote;
-        uint256 fee;
-        uint128 liquidity;
-    }
-
-    struct RemoveLiquidityResponse {
-        uint256 base;
-        uint256 quote;
-        uint256 fee;
-        int256 takerBase;
-        int256 takerQuote;
-    }
-
-    struct ReplaySwapParams {
-        address baseToken;
-        bool isBaseToQuote;
-        bool shouldUpdateState;
-        int256 amount;
-        uint160 sqrtPriceLimitX96;
-        uint24 exchangeFeeRatio;
-        uint24 uniswapFeeRatio;
-        Funding.Growth globalFundingGrowth;
-    }
-
-    /// @param insuranceFundFee = fee * insuranceFundFeeRatio
-    struct ReplaySwapResponse {
-        int24 tick;
-        uint256 fee;
-        uint256 insuranceFundFee;
-    }
 
     struct MintCallbackData {
         address trader;
@@ -65,36 +14,6 @@ interface IOrderBook {
     /// @notice Emitted when the `Exchange` contract address changed
     /// @param exchange The address of exchange contract
     event ExchangeChanged(address indexed exchange);
-
-    /// @notice Add liquidity logic
-    /// @dev Only used by `Positioning` contract
-    /// @param params Add liquidity params, detail on `IOrderBook.AddLiquidityParams`
-    /// @return response Add liquidity response, detail on `IOrderBook.AddLiquidityResponse`
-    function addLiquidity(AddLiquidityParams calldata params) external returns (AddLiquidityResponse memory response);
-
-    /// @notice Remove liquidity logic, only used by `Positioning` contract
-    /// @param params Remove liquidity params, detail on `IOrderBook.RemoveLiquidityParams`
-    /// @return response Remove liquidity response, detail on `IOrderBook.RemoveLiquidityResponse`
-    function removeLiquidity(RemoveLiquidityParams calldata params) external returns (RemoveLiquidityResponse memory);
-
-    /// @dev This is the non-view version of `getLiquidityCoefficientInFundingPayment()`,
-    /// only can be called by `Positioning` contract
-    /// @param trader The trader address
-    /// @param baseToken The base token address
-    /// @param fundingGrowthGlobal The funding growth info, detail on `Funding.Growth`
-    /// @return liquidityCoefficientInFundingPayment the funding payment of all orders/liquidity of a maker
-    function updateFundingGrowthAndLiquidityCoefficientInFundingPayment(
-        address trader,
-        address baseToken,
-        Funding.Growth memory fundingGrowthGlobal
-    ) external returns (int256 liquidityCoefficientInFundingPayment);
-
-    /// @notice Replay the swap and get the swap result (price impact and swap fee),
-    /// only can be called by `Positioning` contract;
-    /// @dev `ReplaySwapResponse.insuranceFundFee = fee * insuranceFundFeeRatio`
-    /// @param params ReplaySwap params, detail on `IOrderBook.ReplaySwapParams`
-    /// @return response The swap result encoded in `ReplaySwapResponse`
-    function replaySwap(ReplaySwapParams memory params) external returns (ReplaySwapResponse memory response);
 
     function updateOrderDebt(
         bytes32 orderId,
@@ -164,17 +83,6 @@ interface IOrderBook {
         address baseToken,
         bool fetchBase
     ) external view returns (uint256);
-
-    /// @notice Get the pending funding payment of all orders in the given market
-    /// @dev This is the view version of `updateFundingGrowthAndLiquidityCoefficientInFundingPayment()`, so only
-    /// part of the funding payment will be returned. Use it with caution because it does not return all the pending
-    /// funding payment of orders. **Normally you won't need to use this function**
-    /// @return liquidityCoefficientInFundingPayment the funding payment of all orders/liquidity of a maker
-    function getLiquidityCoefficientInFundingPayment(
-        address trader,
-        address baseToken,
-        Funding.Growth memory fundingGrowthGlobal
-    ) external view returns (int256 liquidityCoefficientInFundingPayment);
 
     /// @notice Get the pending fees of a order
     /// @param trader The trader address
