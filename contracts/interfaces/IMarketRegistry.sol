fadc0b8e25547b6ff354d4b12982ca345f058ade
shailu-s <emailid462@gmail.com>
2022-06-29 14:15:56 +0530

    VOL-953: Updated market registry interface

--- contracts/interfaces/IMarketRegistry.sol
+++ contracts/interfaces/IMarketRegistry.sol
@@ -4,17 +4,9 @@ pragma abicoder v2;
 
 interface IMarketRegistry {
     struct MarketInfo {
-        address pool;
         uint24 exchangeFeeRatio;
-        uint24 uniswapFeeRatio;
         uint24 insuranceFundFeeRatio;
     }
-
-    /// @notice Emitted when a new market is created.
-    /// @param baseToken The address of the base token
-    /// @param feeRatio Fee ratio of the market
-    /// @param pool The address of the pool
-    event PoolAdded(address indexed baseToken, uint24 indexed feeRatio, address indexed pool);
 
     /// @notice Emitted when the fee ratio of a market is updated.
     /// @param baseToken The address of the base token
@@ -29,12 +21,6 @@ interface IMarketRegistry {
     /// @param maxOrdersPerMarket Max orders per market
     event MaxOrdersPerMarketChanged(uint8 maxOrdersPerMarket);
 
-    /// @dev Add a new pool to the registry.
-    /// @param baseToken The token that the pool is for.
-    /// @param feeRatio The fee ratio for the pool.
-    /// @return pool The address of the pool.
-    function addPool(address baseToken, uint24 feeRatio) external returns (address pool);
-
     /// @dev Set the fee ratio for a pool
     /// @param baseToken The token address of the pool.
     /// @param feeRatio The fee ratio for the pool.
@@ -48,11 +34,6 @@ interface IMarketRegistry {
     /// @dev Set max allowed orders per market
     /// @param maxOrdersPerMarketArg The max allowed orders per market
     function setMaxOrdersPerMarket(uint8 maxOrdersPerMarketArg) external;
-
-    /// @notice Get the pool address (UNIv3 pool) by given base token address
-    /// @param baseToken The address of the base token
-    /// @return pool The address of the pool
-    function getPool(address baseToken) external view returns (address pool);
 
     /// @notice Get the fee ratio of a given market
     /// @dev The ratio is in `1e6` format, that means `1% = 1e4`
@@ -75,15 +56,7 @@ interface IMarketRegistry {
     /// @return quoteToken The address of the quote token
     function getQuoteToken() external view returns (address quoteToken);
 
-    /// @notice Get Uniswap factory address
-    /// @return factory The address of the Uniswap factory
-    function getUniswapV3Factory() external view returns (address factory);
-
     /// @notice Get max allowed orders per market
     /// @return maxOrdersPerMarket The max allowed orders per market
     function getMaxOrdersPerMarket() external view returns (uint8 maxOrdersPerMarket);
-
-    /// @notice Check if a pool exist by given base token address
-    /// @return hasPool True if the pool exist, false otherwise
-    function hasPool(address baseToken) external view returns (bool hasPool);
 }
