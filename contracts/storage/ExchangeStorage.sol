d084c7ad55cb8754e5fb62a8574cbf8ba92b722c
shailu-s <emailid462@gmail.com>
2022-06-29 14:24:38 +0530

    VOL-968: Updated exchange storage

--- contracts/storage/ExchangeStorage.sol
+++ contracts/storage/ExchangeStorage.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.6;
 
-import { Funding } from "../libs/Funding.sol";
-
 /// @notice For future upgrades, do not change ExchangeStorageV1. Create a new
 /// contract which implements ExchangeStorageV1 and following the naming convention
 /// ExchangeStorageVX.
@@ -10,20 +8,13 @@ abstract contract ExchangeStorageV1 {
     address internal _orderBook;
     address internal _accountBalance;
     address internal _PositioningConfig;
-    address internal _markSmaArg;
+    address internal _markPriceOracleArg;
     address internal _exchangeManager;
     address internal _transferManager;
+    address internal _indexPriceOracleArg;
 
-    mapping(address => int24) internal _lastUpdatedTickMap;
     mapping(address => uint256) internal _firstTradedTimestampMap;
     mapping(address => uint256) internal _lastSettledTimestampMap;
-    mapping(address => Funding.Growth) internal _globalFundingGrowthX96Map;
-
-    // key: base token
-    // value: a threshold to limit the price impact per block when reducing or closing the position
-    mapping(address => uint24) internal _maxTickCrossedWithinBlockMap;
-
-    // first key: trader, second key: baseToken
-    // value: the last timestamp when a trader exceeds price limit when closing a position/being liquidated
-    mapping(address => mapping(address => uint256)) internal _lastOverPriceLimitTimestampMap;
+    // mapping basetoken => twpremium(time weighted)
+    mapping(address => int256) internal _globalFundingGrowthMap;
 }
