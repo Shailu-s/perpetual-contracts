90f46bd66e7fa345be48bb48bf3a18496219ab1c
vijay-volmex <vijay@volmexlabs.com>
2022-07-07 18:29:23 +0530

    Add getter for mark price feed address

--- contracts/interfaces/IBaseToken.sol
+++ contracts/interfaces/IBaseToken.sol
@@ -7,4 +7,8 @@ interface IBaseToken {
     /// @notice Get the price feed address
     /// @return priceFeed the current price feed
     function getPriceFeed() external view returns (address priceFeed);
+
+    /// @notice Get the mark price feed address
+    /// @return markPriceFeed the current mark price feed
+    function getMarkPriceFeed() external view returns (address markPriceFeed);
 }
