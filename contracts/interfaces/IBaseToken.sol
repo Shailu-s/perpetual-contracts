bcda7f435d0c76cea003f6846e0caf36bcbcc671
vijay-volmex <vijay@volmexlabs.com>
2022-07-08 14:29:14 +0530

    Add external methods to the interface

--- contracts/interfaces/IBaseToken.sol
+++ contracts/interfaces/IBaseToken.sol
@@ -4,11 +4,16 @@ pragma solidity =0.8.12;
 interface IBaseToken {
     event PriceFeedChanged(address indexed priceFeed);
 
+    function initialize(string memory nameArg, string memory symbolArg, address priceFeedArg) external;
+
+    /// @dev This function is only used for emergency shutdown, to set priceFeed to an emergencyPriceFeed
+    function setPriceFeed(address priceFeedArg) external;
+
+    /// @notice Get the current index price
+    /// @return indexPrice the current index price
+    function getIndexPrice(uint256 interval) external view returns (uint256 indexPrice);
+
     /// @notice Get the price feed address
     /// @return priceFeed the current price feed
     function getPriceFeed() external view returns (address priceFeed);
-
-    /// @notice Get the mark price feed address
-    /// @return markPriceFeed the current mark price feed
-    function getMarkPriceFeed() external view returns (address markPriceFeed);
 }
