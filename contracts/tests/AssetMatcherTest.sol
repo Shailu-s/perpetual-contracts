f3b092ebb77528239a2f8cfb4c45d29e2f06afb2
shailu-s <emailid462@gmail.com>
2022-07-12 09:59:42 +0530

    VOL-988: Added asset matcher test contract

---
+++ contracts/tests/AssetMatcherTest.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import "../matching-engine/AssetMatcher.sol";
+
+contract AssetMatcherTest is Initializable, OwnableUpgradeable, AssetMatcher {
+    function __AssetMatcherTest_init() external initializer {
+        __Ownable_init_unchained();
+    }
+
+    function matchAssetsTest(
+        LibAsset.Asset calldata leftAsset,
+        LibAsset.Asset calldata rightAsset
+    ) external pure returns (LibAsset.Asset memory) {
+        return matchAssets(leftAsset, rightAsset);
+    }
+}
