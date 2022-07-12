b181a05bda6f5d986a592db3bade80c14587b9a2
shailu-s <emailid462@gmail.com>
2022-07-12 09:09:13 +0530

    VOL-988: Added order prepare helper

---
+++ test/order.js
@@ -0,0 +1,53 @@
+const EIP712 = require("./EIP712")
+
+function Asset(virtualToken, value) {
+  return { virtualToken, value }
+}
+
+function Order(maker, makeAsset, taker, takeAsset, salt, deadline, dataType, data) {
+  return {
+    maker,
+    makeAsset,
+    taker,
+    takeAsset,
+    salt,
+    deadline,
+    dataType,
+    data,
+  }
+}
+
+const Types = {
+  Asset: [
+    { name: "virtualToken", type: "address" },
+    { name: "value", type: "uint256" },
+  ],
+  Order: [
+    { name: "maker", type: "address" },
+    { name: "makeAsset", type: "Asset" },
+    { name: "taker", type: "address" },
+    { name: "takeAsset", type: "Asset" },
+    { name: "salt", type: "uint256" },
+    { name: "deadline", type: "uint256" },
+    { name: "dataType", type: "bytes4" },
+    { name: "data", type: "bytes" },
+  ],
+}
+
+async function sign(order, account, verifyingContract) {
+  const chainId = Number(await web3.eth.getChainId())
+  const data = EIP712.createTypeData(
+    {
+      name: "V_PERP",
+      version: "1",
+      chainId,
+      verifyingContract,
+    },
+    "Order",
+    order,
+    Types,
+  )
+  return (await EIP712.signTypedData(web3, account, data)).sig
+}
+
+module.exports = { Asset, Order, sign }
