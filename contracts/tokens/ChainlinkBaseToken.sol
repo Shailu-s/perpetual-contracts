a43e0c4f99816f786d86bc91a8f09b90a641cded
vijay-volmex <vijay@volmexlabs.com>
2022-07-21 18:28:40 +0530

    VOL-994: Move common methods to parent contract

--- contracts/tokens/ChainlinkBaseToken.sol
+++ contracts/tokens/ChainlinkBaseToken.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.6;
 
-import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
-import { IPriceFeed } from "@perp/perp-oracle-contract/contracts/interface/IPriceFeed.sol";
-import { VirtualToken } from "./VirtualToken.sol";
+import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import { IChainlinkIndexPriceOracle } from "../interfaces/IChainlinkIndexPriceOracle.sol";
 import { ParentToken } from "./base/ParentToken.sol";
 
 // never inherit any new stateful contract. never change the orders of parent stateful contracts
@@ -12,50 +11,10 @@ contract ChainlinkBaseToken is ParentToken {
     using SafeMathUpgradeable for uint8;
 
     //
-    // EXTERNAL NON-VIEW
-    //
-
-    function initialize(
-        string memory nameArg,
-        string memory symbolArg,
-        address priceFeedArg
-    ) external initializer {
-        __VirtualToken_init(nameArg, symbolArg);
-
-        uint8 priceFeedDecimals = IPriceFeed(priceFeedArg).decimals();
-
-        // invalid price feed decimals
-        require(priceFeedDecimals <= decimals(), "BT_IPFD");
-
-        _priceFeed = priceFeedArg;
-        _priceFeedDecimals = priceFeedDecimals;
-    }
-
-    /// @dev This function is only used for emergency shutdown, to set priceFeed to an emergencyPriceFeed
-    function setPriceFeed(address priceFeedArg) external override onlyOwner {
-        // ChainlinkPriceFeed uses 8 decimals
-        uint8 priceFeedDecimals = IPriceFeed(priceFeedArg).decimals();
-        // BT_IPFD: Invalid price feed decimals
-        require(priceFeedDecimals <= decimals(), "BT_IPFD");
-
-        _priceFeed = priceFeedArg;
-        _priceFeedDecimals = priceFeedDecimals;
-
-        emit PriceFeedChanged(_priceFeed);
-    }
-
-    //
     // EXTERNAL VIEW
     //
-
-    /// @inheritdoc IIndexPrice
     function getIndexPrice(uint256 interval) external view override returns (uint256) {
-        (, uint256 answer, , , ) = IPriceFeed(_priceFeed).latestRoundData(interval);
-        return _formatDecimals(answer);
-    }
-
-    /// @inheritdoc IVolmexBaseToken
-    function getPriceFeed() external view override returns (address) {
-        return _priceFeed;
+        (, int256 answer, , , ) = IChainlinkIndexPriceOracle(_priceFeed).latestRoundData(interval);
+        return _formatDecimals(uint256(answer)); // TODO: explicit conversion from int256 to uint256
     }
 }
--- contracts/tokens/base/ParentToken.sol
+++ contracts/tokens/base/ParentToken.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.6;
 
-import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
 import { IPriceFeed } from "@perp/perp-oracle-contract/contracts/interface/IPriceFeed.sol";
 import { IIndexPrice } from "../../interfaces/IIndexPrice.sol";
 import { VirtualToken } from "../VirtualToken.sol";
-import { BaseTokenStorage } from "../../storage/BaseTokenStorage.sol";
+import { BaseTokenStorageV1 } from "../../storage/BaseTokenStorage.sol";
 import { IVolmexBaseToken } from "../../interfaces/IVolmexBaseToken.sol";
 
 // never inherit any new stateful contract. never change the orders of parent stateful contracts
-abstract contract ParentBaseToken is IVolmexBaseToken, IIndexPrice, VirtualToken, BaseTokenStorageV1 {
+abstract contract ParentToken is IVolmexBaseToken, IIndexPrice, VirtualToken, BaseTokenStorageV1 {
     using SafeMathUpgradeable for uint256;
     using SafeMathUpgradeable for uint8;
 
@@ -17,18 +17,47 @@ abstract contract ParentBaseToken is IVolmexBaseToken, IIndexPrice, VirtualToken
     // EXTERNAL NON-VIEW
     //
 
+    function initialize(
+        string memory nameArg,
+        string memory symbolArg,
+        address priceFeedArg
+    ) external override initializer {
+        __VirtualToken_init(nameArg, symbolArg);
+
+        uint8 priceFeedDecimals = IPriceFeed(priceFeedArg).decimals();
+
+        // invalid price feed decimals
+        require(priceFeedDecimals <= decimals(), "BT_IPFD");
+
+        _priceFeed = priceFeedArg;
+        _priceFeedDecimals = priceFeedDecimals;
+    }
+
     /// @dev This function is only used for emergency shutdown, to set priceFeed to an emergencyPriceFeed
-    function setPriceFeed(address priceFeedArg) external onlyOwner;
+    function setPriceFeed(address priceFeedArg) external override {
+        // ChainlinkPriceFeed uses 8 decimals
+        // BandPriceFeed uses 18 decimals
+        uint8 priceFeedDecimals = IPriceFeed(priceFeedArg).decimals();
+        // BT_IPFD: Invalid price feed decimals
+        require(priceFeedDecimals <= decimals(), "BT_IPFD");
+
+        _priceFeed = priceFeedArg;
+        _priceFeedDecimals = priceFeedDecimals;
+
+        emit PriceFeedChanged(_priceFeed);
+    }
 
     //
     // EXTERNAL VIEW
     //
 
     /// @inheritdoc IIndexPrice
-    function getIndexPrice(uint256 interval) external view override returns (uint256);
-
+    function getIndexPrice(uint256 interval) external view virtual override(IIndexPrice, IVolmexBaseToken) returns (uint256);
+    
     /// @inheritdoc IVolmexBaseToken
-    function getPriceFeed() external view override returns (address);
+    function getPriceFeed() external view override returns (address) {
+        return _priceFeed;
+    }
 
     //
     // INTERNAL VIEW
