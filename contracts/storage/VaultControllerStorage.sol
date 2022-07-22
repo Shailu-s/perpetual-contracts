d29f1c88a46b1b51de85ef9b6086246912b98c42
shailu-s <emailid462@gmail.com>
2022-07-22 13:25:34 +0530

    VOL-1016: Modified storage for vault controller

--- contracts/storage/VaultControllerStorage.sol
+++ contracts/storage/VaultControllerStorage.sol
@@ -6,5 +6,10 @@ abstract contract VaultControllerStorage {
     
     // key: token of vault
     mapping(address => address) internal _vaultAddress;
+
+    mapping(address => address[]) internal _tradersVaultMap;
     address internal _positioning;
+    address internal _accountBalance;
+    uint8 internal _decimals;
+
 }
