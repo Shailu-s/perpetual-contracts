620d3897b260df4071ee0ffe2303901bced65253
vijay-volmex <vijay@volmexlabs.com>
2022-07-25 16:16:52 +0530

    VOL-995: Mark Price Oracle store observation for different base tokens

--- contracts/oracles/MarkPriceOracle.sol
+++ contracts/oracles/MarkPriceOracle.sol
@@ -18,8 +18,12 @@ contract MarkPriceOracle is Initializable {
 
     // Address of the Exchange contract
     address public exchange;
-    // Array of Observation struct
-    Observation[] public observations;
+
+    // mapping to store index to the address of the baseToken
+    mapping(uint256 => address) public indexToBaseToken;
+
+    // mapping to store baseToken to Observations 
+    mapping(address => Observation[]) public baseTokenToObservations;
 
     // Used to check the caller is Exchange contract
     modifier onlyExchange() {
@@ -32,12 +36,16 @@ contract MarkPriceOracle is Initializable {
      *
      * @param _exchange Address of the Exchange contract
      * @param _priceCumulative Initial price of the asset
+     * @param _asset Address of the asset
      */
-    function initialize(address _exchange, uint256 _priceCumulative) external initializer {
+    function initialize(address _exchange, uint256 _priceCumulative, address _asset) external initializer {
         require(_exchange != address(0), "MarkSMA: Not zero address");
         require(_priceCumulative > 1000000, "MarkSMA: Not decimal precise");
+        require(_asset != address(0), "MarkSMA: Asset address can't be 0");
         exchange = _exchange;
         Observation memory observation = Observation({ timestamp: block.timestamp, priceCumulative: _priceCumulative });
+        indexToBaseToken[0] = _asset;
+        Observation[] storage observations = baseTokenToObservations[_asset];
         observations.push(observation);
     }
 
@@ -45,10 +53,13 @@ contract MarkPriceOracle is Initializable {
      * @notice Used to add price cumulative of an asset at a given timestamp
      *
      * @param _priceCumulative Price of the asset
-     */
-    function addObservation(uint256 _priceCumulative) external onlyExchange {
+     */    
+    function addObservation(uint256 _priceCumulative, uint256 _index) external onlyExchange {
         require(_priceCumulative > 1000000, "MarkSMA: Not decimal precise");
         Observation memory observation = Observation({ timestamp: block.timestamp, priceCumulative: _priceCumulative });
+        address baseTokenAddr = indexToBaseToken[_index];
+        require(baseTokenAddr != address(0), "MarkSMA: Base token not found");
+        Observation[] storage observations = baseTokenToObservations[baseTokenAddr];
         observations.push(observation);
     }
 
@@ -58,7 +69,10 @@ contract MarkPriceOracle is Initializable {
      * @param _twInterval Time in seconds of the range
      * @return priceCumulative The SMA price of the asset
      */
-    function getCumulativePrice(uint256 _twInterval) external view returns (uint256 priceCumulative) {
+    function getCumulativePrice(uint256 _twInterval, uint256 _index) external view returns (uint256 priceCumulative) {
+        address baseTokenAddr = indexToBaseToken[_index];
+        require(baseTokenAddr != address(0), "MarkSMA: Base token not found");
+        Observation[] memory observations = baseTokenToObservations[baseTokenAddr];
         uint256 index = observations.length - 1;
         uint256 initialTimestamp = block.timestamp - _twInterval;
         for (index = observations.length - 1; observations[index].timestamp >= initialTimestamp; index--) {
