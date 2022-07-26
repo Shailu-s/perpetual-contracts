650d3f80b9fb198da98ba7df8597f61bde56999e
shailu-s <emailid462@gmail.com>
2022-07-26 11:20:29 +0530

    VOL-1024: Removed unnecessary interface

--- contracts/interfaces/ITransferProxy.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL - 1.1
-
-pragma solidity =0.8.12;
-
-import "../libs/LibAsset.sol";
-
-interface ITransferProxy {
-    function transfer(LibAsset.Asset calldata asset, address from, address to) external;
-}
