9831ed24f2239d404f81934b076ddade1a9d5aa3
vijay-volmex <vijay@volmexlabs.com>
2022-07-26 14:00:39 +0530

    VOL-995: Update Mark Price Oracle Observations

--- contracts/oracles/MarkPriceOracle.sol
+++ contracts/oracles/MarkPriceOracle.sol
@@ -19,11 +19,14 @@ contract MarkPriceOracle is Initializable {
     // Address of the Exchange contract
     address public exchange;
 
+    // Index count
+    uint64 internal _indexCount;
+
     // mapping to store index to the address of the baseToken
-    mapping(uint256 => address) public indexToBaseToken;
+    mapping(uint64 => address) public baseTokenByIndex;
 
     // mapping to store baseToken to Observations 
-    mapping(address => Observation[]) public baseTokenToObservations;
+    mapping(uint64 => Observation[]) public observationsByIndex;
 
     // Used to check the caller is Exchange contract
     modifier onlyExchange() {
@@ -35,18 +38,35 @@ contract MarkPriceOracle is Initializable {
      * @notice Initialize the contract
      *
      * @param _exchange Address of the Exchange contract
-     * @param _priceCumulative Initial price of the asset
-     * @param _asset Address of the asset
+     * @param _priceCumulative Array of initial prices of the assets
+     * @param _asset Array of addresses of the assets
      */
-    function initialize(address _exchange, uint256 _priceCumulative, address _asset) external initializer {
+    function initialize(address _exchange, uint256[] memory _priceCumulative, address[] memory _asset) external initializer {
         require(_exchange != address(0), "MarkSMA: Not zero address");
-        require(_priceCumulative > 1000000, "MarkSMA: Not decimal precise");
-        require(_asset != address(0), "MarkSMA: Asset address can't be 0");
+
+        uint256 priceCumulativeLength = _priceCumulative.length;
+        uint256 assetLength = _asset.length;
+        require(priceCumulativeLength == assetLength, "MarkSMA: Unequal length of prices & assets");
+
+        for (uint index; index < priceCumulativeLength; index++) {
+            require(_priceCumulative[index] > 1000000, "MarkSMA: Not decimal precise");
+            require(_asset[index] != address(0), "MarkSMA: Asset address can't be 0");
+        }
+
         exchange = _exchange;
-        Observation memory observation = Observation({ timestamp: block.timestamp, priceCumulative: _priceCumulative });
-        indexToBaseToken[0] = _asset;
-        Observation[] storage observations = baseTokenToObservations[_asset];
-        observations.push(observation);
+
+        Observation memory observation;
+        uint64 indexCount = _indexCount;
+
+        for (uint index; index < priceCumulativeLength; index++) {
+            observation = Observation({ timestamp: block.timestamp, priceCumulative: _priceCumulative[index] });
+            baseTokenByIndex[indexCount] = _asset[index];
+            Observation[] storage observations = observationsByIndex[indexCount];
+            observations.push(observation);
+            indexCount++;
+        }
+
+        _indexCount = indexCount;
     }
 
     /**
@@ -54,12 +74,10 @@ contract MarkPriceOracle is Initializable {
      *
      * @param _priceCumulative Price of the asset
      */    
-    function addObservation(uint256 _priceCumulative, uint256 _index) external onlyExchange {
+    function addObservation(uint256 _priceCumulative, uint64 _index) external onlyExchange {
         require(_priceCumulative > 1000000, "MarkSMA: Not decimal precise");
         Observation memory observation = Observation({ timestamp: block.timestamp, priceCumulative: _priceCumulative });
-        address baseTokenAddr = indexToBaseToken[_index];
-        require(baseTokenAddr != address(0), "MarkSMA: Base token not found");
-        Observation[] storage observations = baseTokenToObservations[baseTokenAddr];
+        Observation[] storage observations = observationsByIndex[_index];
         observations.push(observation);
     }
 
@@ -69,10 +87,8 @@ contract MarkPriceOracle is Initializable {
      * @param _twInterval Time in seconds of the range
      * @return priceCumulative The SMA price of the asset
      */
-    function getCumulativePrice(uint256 _twInterval, uint256 _index) external view returns (uint256 priceCumulative) {
-        address baseTokenAddr = indexToBaseToken[_index];
-        require(baseTokenAddr != address(0), "MarkSMA: Base token not found");
-        Observation[] memory observations = baseTokenToObservations[baseTokenAddr];
+    function getCumulativePrice(uint256 _twInterval, uint64 _index) external view returns (uint256 priceCumulative) {
+        Observation[] memory observations = observationsByIndex[_index];
         uint256 index = observations.length - 1;
         uint256 initialTimestamp = block.timestamp - _twInterval;
         for (index = observations.length - 1; observations[index].timestamp >= initialTimestamp; index--) {
