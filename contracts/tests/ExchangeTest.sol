a2464b3e0114ade22d7e89d0b9c11f0dcc0b1455
vijay-volmex <vijay@volmexlabs.com>
2022-07-29 18:10:17 +0530

    VOL-995: Add mock contract to test MarkPriceOracle

--- contracts/tests/ExchangeTest.sol
+++ contracts/tests/ExchangeTest.sol
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.6;
 
+import "contracts/oracles/MarkPriceOracle.sol";
+
 contract ExchangeTest {
+    MarkPriceOracle public markPriceOracle;
+
+    function setMarkPriceOracle(MarkPriceOracle _markPriceOracle) external {
+        markPriceOracle = _markPriceOracle;
+    }
+
+    function addObservation(uint256 _priceCumulative, uint64 _index) external {
+        markPriceOracle.addObservation(_priceCumulative, _index);
+    }
 }
