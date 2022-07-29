e52e3f8dd030877b96ab0a3bf6bad1689a7e166f
vijay-volmex <vijay@volmexlabs.com>
2022-07-29 18:11:41 +0530

    VOL-995: Add unit test cases for Mark price oracle

--- test/oracles/MarkPriceOracle.test.ts
+++ test/oracles/MarkPriceOracle.test.ts
@@ -7,10 +7,19 @@ describe('MarkPriceOracle', function () {
   let markPriceOracle;
   let ExchangeTest;
   let exchangeTest;
+  let factory;
+  let Factory;
+  let volmexBaseToken;
+  let VolmexBaseToken;
+  let indexPriceOracle;
+  let IndexPriceOracle;
 
   this.beforeAll(async () => {
     MarkPriceOracle = await ethers.getContractFactory("MarkPriceOracle");
     ExchangeTest = await ethers.getContractFactory("ExchangeTest");
+    Factory = await ethers.getContractFactory("Factory");
+    VolmexBaseToken = await ethers.getContractFactory("VolmexBaseToken");
+    IndexPriceOracle = await ethers.getContractFactory("IndexPriceOracle");
   });
 
   beforeEach(async () => {
@@ -18,17 +27,51 @@ describe('MarkPriceOracle', function () {
 
     exchangeTest = await ExchangeTest.deploy();
 
-    markPriceOracle = await upgrades.deployProxy(
-      MarkPriceOracle,
+    indexPriceOracle = await upgrades.deployProxy(
+      IndexPriceOracle,
       [
-        exchangeTest.address,
-        [],
-        [],
+        owner.address,
       ],
       { 
         initializer: "initialize",
       }
     );
+
+    volmexBaseToken = await upgrades.deployProxy(
+      VolmexBaseToken,
+      [
+        "VolmexBaseToken", // nameArg
+        "VBT", // symbolArg,
+        indexPriceOracle.address, // priceFeedArg
+      ],
+      {
+        initializer: "initialize",
+      }
+    );
+
+    factory = await upgrades.deployProxy(
+      Factory,
+      [
+        volmexBaseToken.address, // implementation
+      ],
+      {
+        initializer: "initialize"
+      }
+    );
+
+    markPriceOracle = await upgrades.deployProxy(
+      MarkPriceOracle,
+      [
+        exchangeTest.address,
+        [10000000],
+        [volmexBaseToken.address],
+      ],
+      { 
+        initializer: "initialize",
+      }
+    );
+
+    await exchangeTest.setMarkPriceOracle(markPriceOracle.address);
   });
 
   describe('Deployment', function () {   
@@ -37,14 +80,111 @@ describe('MarkPriceOracle', function () {
         MarkPriceOracle,
         [
           exchangeTest.address,
-          [],
-          []
+          [10000000],
+          [volmexBaseToken.address],
         ],
         { 
           initializer: "initialize",
         }
       );
       expect(receipt.confirmations).not.equal(0);
+    });
+
+    it("Should fail to deploy if exchange address is 0", async () => {
+      await expect(
+        upgrades.deployProxy(
+          MarkPriceOracle,
+          [
+            '0x0000000000000000000000000000000000000000',
+            [10000000],
+            [volmexBaseToken.address],
+          ],
+          { 
+            initializer: "initialize",
+          }
+        ),
+      ).to.be.revertedWith("MarkSMA: Not zero address");
+    });
+
+    it("Should fail to deploy if length of arrays is unequal", async () => {
+      await expect(
+        upgrades.deployProxy(
+          MarkPriceOracle,
+          [
+            exchangeTest.address,
+            [10000000],
+            [],
+          ],
+          { 
+            initializer: "initialize",
+          }
+        ),
+      ).to.be.revertedWith("MarkSMA: Unequal length of prices & assets");
+    });
+
+    it("Should fail to deploy when price is <= 1000000", async () => {
+      await expect(
+        upgrades.deployProxy(
+          MarkPriceOracle,
+          [
+            exchangeTest.address,
+            [100000],
+            [volmexBaseToken.address],
+          ],
+          { 
+            initializer: "initialize",
+          }
+        ),
+      ).to.be.revertedWith("MarkSMA: Not decimal precise");
+    });
+
+    it("Should fail to deploy when asset address is 0", async () => {
+      await expect(
+        upgrades.deployProxy(
+          MarkPriceOracle,
+          [
+            exchangeTest.address,
+            [10000000],
+            ['0x0000000000000000000000000000000000000000'],
+          ],
+          { 
+            initializer: "initialize",
+          }
+        ),
+      ).to.be.revertedWith("MarkSMA: Asset address can't be 0");
+    });
+  });
+
+  describe("Add Observation", async () => {
+    it("Should add observation", async () => {
+      for (let i = 0; i < 9; i++) {
+        await exchangeTest.addObservation(10000000, 0);
+      }
+
+      const txn = await markPriceOracle.getCumulativePrice(10000000, 0);
+      // Total cumulative = 100000000 (One added while MarkPriceOracle deployment & rest 9 using loop)
+      // Total observations = 10
+      // Cumulative = (Total cumulative / Total observations) 
+      //            = (100000000 / 10) 
+      //            = 10000000
+      expect(Number(txn)).equal(10000000);
+    });
+
+    it("Should fail to add observation when caller is not exchange", async () => {
+      await expect(
+        markPriceOracle.addObservation(1000000, 0)
+      ).to.be.revertedWith("MarkSMA: Not Exchange");
+    });
+
+    it("Should fail to add observation when cumulative price is <= 1000000", async () => {
+      await expect(
+        exchangeTest.addObservation(1000000, 0)
+      ).to.be.revertedWith("MarkSMA: Not decimal precise");
+    });
+
+    it("Should get cumulative price", async () => {
+      const txn = await markPriceOracle.getCumulativePrice(1000000, 0);
+      expect(Number(txn)).equal(10000000);
     });
   });
 });
