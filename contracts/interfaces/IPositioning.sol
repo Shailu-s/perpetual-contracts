8bd88d65655354ade03913c3dc8b9fbcd619d9b5
shailu-s <emailid462@gmail.com>
2022-08-04 09:37:06 +0530

    VOL-1054: Updated positioning interface

--- contracts/interfaces/IPositioning.sol
+++ contracts/interfaces/IPositioning.sol
@@ -1,134 +1,20 @@
 // SPDX-License-Identifier: BUSL - 1.1
 pragma solidity =0.8.12;
+import "../libs/LibOrder.sol";
+import "../libs/LibFill.sol";
+import "../libs/LibDeal.sol";
 
 interface IPositioning {
-    /// @param useTakerBalance only accept false now
-    struct AddLiquidityParams {
-        address baseToken;
-        uint256 base;
-        uint256 quote;
-        int24 lowerTick;
-        int24 upperTick;
-        uint256 minBase;
-        uint256 minQuote;
-        bool useTakerBalance;
-        uint256 deadline;
-    }
-
-    /// @param liquidity collect fee when 0
-    struct RemoveLiquidityParams {
-        address baseToken;
-        int24 lowerTick;
-        int24 upperTick;
-        uint128 liquidity;
-        uint256 minBase;
-        uint256 minQuote;
-        uint256 deadline;
-    }
-
-    struct AddLiquidityResponse {
-        uint256 base;
-        uint256 quote;
-        uint256 fee;
-        uint256 liquidity;
-    }
-
-    struct RemoveLiquidityResponse {
-        uint256 base;
-        uint256 quote;
-        uint256 fee;
-    }
-
-    /// @param oppositeAmountBound
-    // B2Q + exact input, want more output quote as possible, so we set a lower bound of output quote
-    // B2Q + exact output, want less input base as possible, so we set a upper bound of input base
-    // Q2B + exact input, want more output base as possible, so we set a lower bound of output base
-    // Q2B + exact output, want less input quote as possible, so we set a upper bound of input quote
-    // when it's set to 0, it will disable slippage protection entirely regardless of exact input or output
-    // when it's over or under the bound, it will be reverted
-    /// @param sqrtPriceLimitX96
-    // B2Q: the price cannot be less than this value after the swap
-    // Q2B: the price cannot be greater than this value after the swap
-    // it will fill the trade until it reaches the price limit but WON'T REVERT
-    // when it's set to 0, it will disable price limit;
-    // when it's 0 and exact output, the output amount is required to be identical to the param amount
-    struct OpenPositionParams {
-        address baseToken;
-        bool isBaseToQuote;
-        bool isExactInput;
-        uint256 amount;
-        uint256 oppositeAmountBound;
-        uint256 deadline;
-        uint160 sqrtPriceLimitX96;
-        bytes32 referralCode;
-    }
-
-    struct ClosePositionParams {
-        address baseToken;
-        uint160 sqrtPriceLimitX96;
-        uint256 oppositeAmountBound;
-        uint256 deadline;
-        bytes32 referralCode;
-    }
-
-    struct CollectPendingFeeParams {
-        address trader;
-        address baseToken;
-        int24 lowerTick;
-        int24 upperTick;
-    }
-
-    struct PositionParams {
-        address trader;
-        uint64 deadline;
-        bool isShort;
-        bool isMaker;
-        address baseToken;
-        uint256 amount;
-        uint256 salt;
+    struct SwapResponse {
+        address leftTraderBaseToken;
+        address RightTraderBaseToken;
+        LibFill.FillResult newFill;
+        LibDeal.DealData dealData;
     }
 
     /// @notice Emitted when open position with non-zero referral code
     /// @param referralCode The referral code by partners
     event ReferredPositionChanged(bytes32 indexed referralCode);
-
-    /// @notice Emitted when taker position is being liquidated
-    /// @param trader The trader who has been liquidated
-    /// @param baseToken Virtual base token(ETH, BTC, etc...) address
-    /// @param positionNotional The cost of position
-    /// @param positionSize The size of position
-    /// @param liquidationFee The fee of liquidate
-    /// @param liquidator The address of liquidator
-    event PositionLiquidated(
-        address indexed trader,
-        address indexed baseToken,
-        uint256 positionNotional,
-        uint256 positionSize,
-        uint256 liquidationFee,
-        address liquidator
-    );
-
-    /// @notice Emitted when maker's liquidity of a order changed
-    /// @param maker The one who provide liquidity
-    /// @param baseToken The address of virtual base token(ETH, BTC, etc...)
-    /// @param quoteToken The address of virtual USD token
-    /// @param lowerTick The lower tick of the position in which to add liquidity
-    /// @param upperTick The upper tick of the position in which to add liquidity
-    /// @param base The amount of base token added (> 0) / removed (< 0) as liquidity; fees not included
-    /// @param quote The amount of quote token added ... (same as the above)
-    /// @param liquidity The amount of liquidity unit added (> 0) / removed (< 0)
-    /// @param quoteFee The amount of quote token the maker received as fees
-    event LiquidityChanged(
-        address indexed maker,
-        address indexed baseToken,
-        address indexed quoteToken,
-        int24 lowerTick,
-        int24 upperTick,
-        int256 base,
-        int256 quote,
-        int128 liquidity,
-        uint256 quoteFee
-    );
 
     /// @notice Emitted when taker's position is being changed
     /// @param trader Trader address
@@ -137,15 +23,13 @@ interface IPositioning {
     /// @param exchangedPositionNotional The cost of position, include fee
     /// @param fee The fee of open/close position
     /// @param openNotional The cost of open/close position, < 0: long, > 0: short
-    /// @param realizedPnl The realized Pnl after open/close position
     event PositionChanged(
         address indexed trader,
         address indexed baseToken,
         int256 exchangedPositionSize,
         int256 exchangedPositionNotional,
         uint256 fee,
-        int256 openNotional,
-        int256 realizedPnl
+        int256 openNotional
     );
 
     /// @notice Emitted when settling a trader's funding payment
@@ -164,26 +48,20 @@ interface IPositioning {
     function settleAllFunding(address trader) external;
 
     /// @notice Trader can call `openPosition` to long/short on baseToken market
-    /// @param positionLeft PositionParams struct
-    /// @param positionRight PositionParams struct
-    /// @return base The amount of baseToken the taker got or spent
-    /// @return quote The amount of quoteToken the taker got or spent
+    /// @param orderLeft PositionParams struct
+    /// @param orderRight PositionParams struct
     function openPosition(
-        PositionParams memory positionLeft,
+        LibOrder.Order memory orderLeft,
         bytes memory signatureLeft,
-        PositionParams memory positionRight,
+        LibOrder.Order memory orderRight,
         bytes memory signatureRight
-    ) external returns (uint256 base, uint256 quote);
+    ) external returns (SwapResponse memory);
 
     /// @notice Get account value of trader
     /// @dev accountValue = totalCollateralValue + totalUnrealizedPnl, in 18 decimals
     /// @param trader The address of trader
     /// @return accountValue The account value of trader
     function getAccountValue(address trader) external view returns (int256);
-
-    /// @notice Get QuoteToken address
-    /// @return quoteToken The quote token address
-    function getQuoteToken() external view returns (address quoteToken);
 
     /// @notice Get PositioningConfig address
     /// @return PositioningConfig PositioningConfig address
@@ -202,15 +80,7 @@ interface IPositioning {
     /// @return exchange `Exchange` address
     function getExchange() external view returns (address exchange);
 
-    /// @notice Get `OrderBook` address
-    /// @return orderBook `OrderBook` address
-    function getOrderBook() external view returns (address orderBook);
-
     /// @notice Get AccountBalance address
     /// @return accountBalance `AccountBalance` address
     function getAccountBalance() external view returns (address accountBalance);
-
-    /// @notice Get `InsuranceFund` address
-    /// @return insuranceFund `InsuranceFund` address
-    function getInsuranceFund() external view returns (address insuranceFund);
 }
