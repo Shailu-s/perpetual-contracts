e1b87eacaf5423dbec13fb68e708353789b036fb
shailu-s <emailid462@gmail.com>
2022-08-05 09:28:02 +0530

    VOL-1066: Deploy vault controller and positoning with factory

--- contracts/tokens/factory/PerpFactory.sol
+++ contracts/tokens/factory/PerpFactory.sol
@@ -5,21 +5,31 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
 import "../VolmexBaseToken.sol";
+import "../../orderbook/Positioning.sol";
 import "../../helpers/BlockContext.sol";
 import "../../interfaces/IVolmexBaseToken.sol";
 import "../../interfaces/IVaultController.sol";
-import "../../../contracts/orderbook/VaultController.sol";
+import "../../interfaces/IPositioning.sol";
+import "../../orderbook/VaultController.sol";
 
 /**
  * @title Factory Contract
  * @author volmex.finance [security@volmexlabs.com]
  */
 contract PerpFactory is Initializable, BlockContext {
+    event PerpSystemCreated(uint256 indexed perpIndex, address indexed positioning, address indexed vaultController);
+
     // Volatility token implementation contract for factory
     address public tokenImplementation;
 
     // Vault Controller implementation contract for factory
     address public vaultControllerImplementation;
+
+    // Address of positioning contract implementation
+    address public positioningImplementation;
+
+    // Address of vault contract implementation
+    address public vaultImplementation;
 
     // To store the address of volatility.
     mapping(uint256 => address) public tokenByIndex;
@@ -27,11 +37,14 @@ contract PerpFactory is Initializable, BlockContext {
     // Mapping to store VaultControllers by index
     mapping(uint256 => address) public vaultControllersByIndex;
 
+    // Store the addresses of positioning contract by index
+    mapping(uint256 => IPositioning) public positioningByIndex;
+
     // Used to store the address and name of volatility at a particular _index (incremental state of 1)
     uint256 public tokenIndexCount;
 
-    // Used to store the address and name of VaultController at a particular _index (incremental state of 1)
-    uint256 public vaultControllerIndexCount;
+    // Used to store the address of perp ecosystem at a particular _index (incremental state of 1)
+    uint256 public perpIndexCount;
 
     // Used to store a boolean value if blockchain is ZkSync
     bool public isZkSync;
@@ -39,14 +52,19 @@ contract PerpFactory is Initializable, BlockContext {
     /**
      * @notice Get the address of tokenImplementation contracts instance.
      */
-    function initialize(address _tokenImplementation, address _vaultControllerImplementation) external initializer {
+    function initialize(
+        address _tokenImplementation,
+        address _vaultControllerImplementation,
+        address _vaultImplementation
+    ) external initializer {
         tokenImplementation = _tokenImplementation;
         vaultControllerImplementation = _vaultControllerImplementation;
+        vaultImplementation = _vaultImplementation;
 
         // Get networkId & check for ZkSync
         uint256 networkId = _networkId();
 
-        // TODO: Update ZkSync networkId (currently using 280)
+        // TODO: Add check for zkSync mainnet as well
         isZkSync = (networkId == 280) ? true : false;
     }
 
@@ -81,36 +99,50 @@ contract PerpFactory is Initializable, BlockContext {
         return address(volmexBaseToken);
     }
 
-    function cloneVaultController(
-        address _positioningArg,
+    function clonePerpEcosystem(
         address _positioningConfig,
-        address _accountBalanceArg,
-        address _vaultImplementationArg
-    ) external returns (address) {
-        IVaultController vaultController;
+        address _quoteToken,
+        address _exchange,
+        address _accountBalance,
+        address _insuranceFund
+    ) external returns (IPositioning positioning, IVaultController vaultController) {
+        positioning = _clonePositioning(_positioningConfig, _quoteToken, _exchange, _accountBalance, _insuranceFund);
+        vaultController = _cloneVaultController(address(positioning), _positioningConfig, _accountBalance);
 
+        emit PerpSystemCreated(perpIndexCount, address(positioning), address(vaultController));
+        perpIndexCount++;
+    }
+
+    function _cloneVaultController(
+        address _positioning,
+        address _positioningConfig,
+        address _accountBalance
+    ) internal returns (IVaultController vaultController) {
         if (isZkSync) {
             vaultController = new VaultController();
-            vaultController.initialize(
-                _positioningArg,
-                _positioningConfig,
-                _accountBalanceArg,
-                _vaultImplementationArg
-            );
         } else {
-            bytes32 salt = keccak256(abi.encodePacked(vaultControllerIndexCount, _vaultImplementationArg));
-
+            bytes32 salt = keccak256(abi.encodePacked(perpIndexCount, vaultImplementation));
             vaultController = IVaultController(Clones.cloneDeterministic(vaultControllerImplementation, salt));
-
-            vaultController.initialize(
-                _positioningArg,
-                _positioningConfig,
-                _accountBalanceArg,
-                _vaultImplementationArg
-            );
         }
-        vaultControllersByIndex[vaultControllerIndexCount] = address(vaultController);
-        vaultControllerIndexCount++;
-        return address(vaultController);
+
+        vaultController.initialize(_positioning, _positioningConfig, _accountBalance, vaultImplementation);
+        vaultControllersByIndex[perpIndexCount] = address(vaultController);
+    }
+
+    function _clonePositioning(
+        address _positioningConfig,
+        address _quoteToken,
+        address _exchange,
+        address _accountBalance,
+        address _insuranceFund
+    ) internal returns (IPositioning positioning) {
+        if (isZkSync) {
+            positioning = new Positioning();
+        } else {
+            bytes salt = keccak256(abi.encodePacked(perpIndexCount, _positioningConfig));
+            positioning = IPositioning(Clones.cloneDeterministic(positioningImplementation, salt));
+        }
+        positioning.initialize(_positioningConfig, _quoteToken, _exchange, _accountBalance, _insuranceFund);
+        positioningByIndex[perpIndexCount] = positioning;
     }
 }
