659d3f55549047dd18864dcce4c3192fb0423492
shailu-s <emailid462@gmail.com>
2022-08-09 16:21:21 +0530

    VOL-1066: Update the factory clone method calls

--- contracts/tokens/factory/PerpFactory.sol
+++ contracts/tokens/factory/PerpFactory.sol
@@ -1,25 +1,33 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: BUSL - 1.1
 pragma solidity =0.8.12;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
 import "../VolmexBaseToken.sol";
-import "../../orderbook/Positioning.sol";
+import "../VolmexQuoteToken.sol";
 import "../../helpers/BlockContext.sol";
+import "../../orderbook/Positioning.sol";
+import "../../orderbook/VaultController.sol";
+import "../../orderbook/AccountBalance.sol";
 import "../../interfaces/IVolmexBaseToken.sol";
 import "../../interfaces/IVaultController.sol";
 import "../../interfaces/IPositioning.sol";
-import "../../orderbook/VaultController.sol";
-import "../../orderbook/AccountBalance.sol";
+import "../../interfaces/IVolmexQuoteToken.sol";
 
 /**
  * @title Factory Contract
  * @author volmex.finance [security@volmexlabs.com]
  */
 contract PerpFactory is Initializable, BlockContext {
-    event PerpSystemCreated(uint256 indexed perpIndex, address indexed positioning, address indexed vaultController);
+    event PerpSystemCreated(
+        uint256 indexed perpIndex,
+        address positioning,
+        address vaultController,
+        address accountBalance
+    );
     event VaultCreated(address indexed vault, address indexed token);
+    event TokenCreated(uint256 indexed tokenIndex, address indexed token);
 
     // Volatility token implementation contract for factory
     address public tokenImplementation;
@@ -37,16 +45,25 @@ contract PerpFactory is Initializable, BlockContext {
     address public accountBalanceImplementation;
 
     // To store the address of volatility.
-    mapping(uint256 => address) public tokenByIndex;
+    mapping(uint256 => IVolmexBaseToken) public baseTokenByIndex;
+
+    // To store the address of collateral.
+    mapping(uint256 => IVolmexQuoteToken) public quoteTokenByIndex;
 
     // Mapping to store VaultControllers by index
-    mapping(uint256 => address) public vaultControllersByIndex;
+    mapping(uint256 => IVaultController) public vaultControllersByIndex;
 
     // Store the addresses of positioning contract by index
     mapping(uint256 => IPositioning) public positioningByIndex;
 
+    // Store the addresses of account balance by index
+    mapping(uint256 => address) public accountBalanceByIndex;
+
     // Used to store the address and name of volatility at a particular _index (incremental state of 1)
-    uint256 public tokenIndexCount;
+    uint256 public baseTokenIndexCount;
+
+    // Used to store the address and name of collateral at a particular _index (incremental state of 1)
+    uint256 public quoteTokenIndexCount;
 
     // Used to store the address of perp ecosystem at a particular _index (incremental state of 1)
     uint256 public perpIndexCount;
@@ -58,7 +75,7 @@ contract PerpFactory is Initializable, BlockContext {
     bool public isZkSync;
 
     /**
-     * @notice Get the address of tokenImplementation contracts instance.
+     * @notice Intializes the Factory and stores the implementations
      */
     function initialize(
         address _tokenImplementation,
@@ -81,36 +98,64 @@ contract PerpFactory is Initializable, BlockContext {
     }
 
     /**
-     * @notice Clones the position token - { returns position token address }
+     * @notice Clones the base token - { returns base token address }
      *
-     * @dev Generates a salt using tokenIndexCount, token name and token symbol
-     * @dev Clone the position token implementation with a salt make it deterministic
-     * @dev Initializes the position token
+     * @dev Generates a salt using baseTokenIndexCount, token name and token symbol
+     * @dev Clone the base token implementation with a salt make it deterministic
+     * @dev Initializes the base token
      *
-     * @param _name is the name of volatility token
-     * @param _symbol is the symbol of volatility token
+     * @param _name is the name of base token
+     * @param _symbol is the symbol of base token
+     * @param _priceFeed is the address of referenced price oracle
      */
     function cloneBaseToken(
         string memory _name,
         string memory _symbol,
         address _priceFeed
-    ) external returns (address) {
-        IVolmexBaseToken volmexBaseToken;
+    ) external returns (IVolmexBaseToken volmexBaseToken) {
         if (isZkSync) {
             volmexBaseToken = new VolmexBaseToken();
-
-            volmexBaseToken.initialize(_name, _symbol, _priceFeed, true);
         } else {
-            bytes32 salt = keccak256(abi.encodePacked(tokenIndexCount, _name, _symbol));
-
+            bytes32 salt = keccak256(abi.encodePacked(baseTokenIndexCount, _name, _symbol));
             volmexBaseToken = VolmexBaseToken(Clones.cloneDeterministic(tokenImplementation, salt));
-            volmexBaseToken.initialize(_name, _symbol, _priceFeed, true);
         }
-        tokenByIndex[tokenIndexCount] = address(volmexBaseToken);
-        tokenIndexCount++;
-        return address(volmexBaseToken);
+        volmexBaseToken.initialize(_name, _symbol, _priceFeed, true);
+        baseTokenByIndex[baseTokenIndexCount] = volmexBaseToken;
+        emit TokenCreated(baseTokenIndexCount, address(volmexBaseToken));
+
+        baseTokenIndexCount++;
     }
 
+    /**
+     * @notice Clones the quote token - { returns quote token address }
+     *
+     * @dev Generates a salt using quoteTokenIndexCount, token name and token symbol
+     * @dev Clone the quote token implementation with a salt make it deterministic
+     * @dev Initializes the quote token
+     *
+     * @param _name is the name of quote token
+     * @param _symbol is the symbol of quote token
+     */
+    function cloneQuoteToken(
+        string memory _name,
+        string memory _symbol
+    ) external returns (IVolmexQuoteToken volmexQuoteToken) {
+        if (isZkSync) {
+            volmexQuoteToken = new VolmexQuoteToken();
+        } else {
+            bytes32 salt = keccak256(abi.encodePacked(quoteTokenIndexCount, _name, _symbol));
+            volmexQuoteToken = IVolmexQuoteToken(Clones.cloneDeterministic(tokenImplementation, salt));
+        }
+        volmexQuoteToken.initialize(_name, _symbol, false);
+        quoteTokenByIndex[quoteTokenIndexCount] = volmexQuoteToken;
+        emit TokenCreated(quoteTokenIndexCount, address(volmexQuoteToken));
+
+        quoteTokenIndexCount++;
+    }
+
+    /**
+     * @notice Clones the vault
+     */
     function cloneVault(
         address _token,
         bool isEthVault,
@@ -119,8 +164,8 @@ contract PerpFactory is Initializable, BlockContext {
         address _vaultImplementation,
         uint256 _vaultControllerIndex
     ) external returns (IVault vault) {
-        address vaultControllerAddr = vaultControllersByIndex[_vaultControllerIndex];
-        require(vaultControllerAddr != address(0), "PerpFactory: Vault Controller Not Found");
+        IVaultController vaultController = vaultControllersByIndex[_vaultControllerIndex];
+        require(address(vaultController) != address(0), "PerpFactory: Vault Controller Not Found");
 
         if (isZkSync) {
             vault = new Vault();
@@ -128,23 +173,41 @@ contract PerpFactory is Initializable, BlockContext {
             bytes32 salt = keccak256(abi.encodePacked(_token, vaultIndexCount));
             vault = Vault(Clones.cloneDeterministic(_vaultImplementation, salt));
         }
-        vault.initialize(_positioningConfig, _accountBalance, _token, vaultControllerAddr, isEthVault);
+        vault.initialize(_positioningConfig, _accountBalance, _token, address(vaultController), isEthVault);
         vaultIndexCount++;
-        IVaultController(vaultControllerAddr).registerVault(address(vault), _token);
+
+        vaultController.registerVault(address(vault), _token);
         emit VaultCreated(address(vault), _token);
     }
 
+    /**
+     * @notice Clones the complete perpetual ecosystem
+     */
     function clonePerpEcosystem(
         address _positioningConfig,
         address _quoteToken,
         address _exchange,
-        address _accountBalance,
-        address _insuranceFund
-    ) external returns (IPositioning positioning, IVaultController vaultController) {
-        positioning = _clonePositioning(_positioningConfig, _quoteToken, _exchange, _accountBalance, _insuranceFund);
-        vaultController = _cloneVaultController(address(positioning), _positioningConfig, _accountBalance);
+        address _insuranceFund,
+        address _orderBook
+    )
+        external
+        returns (
+            IPositioning positioning,
+            IVaultController vaultController,
+            IAccountBalance accountBalance
+        )
+    {
+        accountBalance = _cloneAccountBalance(_positioningConfig, _orderBook);
+        positioning = _clonePositioning(
+            _positioningConfig,
+            _quoteToken,
+            _exchange,
+            address(accountBalance),
+            _insuranceFund
+        );
+        vaultController = _cloneVaultController(address(positioning), _positioningConfig, address(accountBalance));
 
-        emit PerpSystemCreated(perpIndexCount, address(positioning), address(vaultController));
+        emit PerpSystemCreated(perpIndexCount, address(positioning), address(vaultController), address(accountBalance));
         perpIndexCount++;
     }
 
@@ -152,7 +215,7 @@ contract PerpFactory is Initializable, BlockContext {
         address _positioning,
         address _positioningConfig,
         address _accountBalance
-    ) internal returns (IVaultController vaultController) {
+    ) private returns (IVaultController vaultController) {
         if (isZkSync) {
             vaultController = new VaultController();
         } else {
@@ -161,7 +224,7 @@ contract PerpFactory is Initializable, BlockContext {
         }
 
         vaultController.initialize(_positioning, _positioningConfig, _accountBalance, vaultImplementation);
-        vaultControllersByIndex[perpIndexCount] = address(vaultController);
+        vaultControllersByIndex[perpIndexCount] = vaultController;
     }
 
     function _clonePositioning(
@@ -170,7 +233,7 @@ contract PerpFactory is Initializable, BlockContext {
         address _exchange,
         address _accountBalance,
         address _insuranceFund
-    ) internal returns (IPositioning positioning) {
+    ) private returns (IPositioning positioning) {
         if (isZkSync) {
             positioning = new Positioning();
         } else {
@@ -179,5 +242,19 @@ contract PerpFactory is Initializable, BlockContext {
         }
         positioning.initialize(_positioningConfig, _quoteToken, _exchange, _accountBalance, _insuranceFund);
         positioningByIndex[perpIndexCount] = positioning;
+    }
+
+    function _cloneAccountBalance(address _positioningConfig, address _orderBook)
+        private
+        returns (IAccountBalance accountBalance)
+    {
+        if (isZkSync) {
+            accountBalance = new AccountBalance();
+        } else {
+            bytes32 salt = keccak256(abi.encodePacked(perpIndexCount, _positioningConfig));
+            accountBalance = IAccountBalance(Clones.cloneDeterministic(accountBalanceImplementation, salt));
+        }
+        accountBalance.initialize(_positioningConfig, _orderBook);
+        accountBalanceByIndex[perpIndexCount] = address(accountBalance);
     }
 }
