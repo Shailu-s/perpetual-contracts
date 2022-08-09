9dc9d99112f34ada1c11547574b3aef861d0fa52
shailu-s <emailid462@gmail.com>
2022-08-09 16:20:41 +0530

    VOL-1066: Update quote token impl and added interface

---
+++ contracts/interfaces/IVolmexQuoteToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BUSL - 1.1
+pragma solidity =0.8.12;
+
+import "./IVirtualToken.sol";
+
+interface IVolmexQuoteToken is IVirtualToken {
+    function initialize(string memory nameArg, string memory symbolArg, bool isBaseArg) external;
+}
--- contracts/tokens/VolmexQuoteToken.sol
+++ contracts/tokens/VolmexQuoteToken.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity =0.8.12;
-import { VirtualToken } from "./VirtualToken.sol";
 
-contract QuoteToken is VirtualToken {
+import "./VirtualToken.sol";
+import "../interfaces/IVolmexQuoteToken.sol";
+
+contract VolmexQuoteToken is IVolmexQuoteToken, VirtualToken {
     function initialize(string memory nameArg, string memory symbolArg, bool isBaseArg) external initializer {
         __VirtualToken_init(nameArg, symbolArg, isBaseArg);
     }
