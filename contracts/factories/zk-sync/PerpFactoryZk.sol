713f29513f8e9165033e2e6f3c8afe039282730b
shailu-s <emailid462@gmail.com>
2022-08-17 14:32:23 +0530

    VOL-1089: Separated token and perp deployment

--- contracts/factories/zk-sync/PerpFactoryZk.sol
+++ contracts/factories/zk-sync/PerpFactoryZk.sol
@@ -4,28 +4,18 @@ pragma solidity =0.8.12;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
-import "../tokens/VolmexBaseToken.sol";
-import "../tokens/VolmexQuoteToken.sol";
-import "../orderbook/Positioning.sol";
-import "../orderbook/VaultController.sol";
-import "../orderbook/AccountBalance.sol";
-import "../interfaces/IVolmexBaseToken.sol";
-import "../interfaces/IVaultController.sol";
-import "../interfaces/IPositioning.sol";
-import "../interfaces/IVolmexQuoteToken.sol";
-import "../interfaces/IPerpFactory.sol";
+import "../../orderbook/Positioning.sol";
+import "../../orderbook/VaultController.sol";
+import "../../orderbook/AccountBalance.sol";
+import "../../interfaces/IVaultController.sol";
+import "../../interfaces/IPositioning.sol";
+import "../../interfaces/IPerpFactory.sol";
 
 /**
  * @title Factory Contract for zkSync
  * @author volmex.finance [security@volmexlabs.com]
  */
 contract PerpFactoryZk is IPerpFactory, Initializable {
-    // To store the address of volatility.
-    mapping(uint256 => IVolmexBaseToken) public baseTokenByIndex;
-
-    // To store the address of collateral.
-    mapping(uint256 => IVolmexQuoteToken) public quoteTokenByIndex;
-
     // Mapping to store VaultControllers by index
     mapping(uint256 => IVaultController) public vaultControllersByIndex;
 
@@ -35,68 +25,11 @@ contract PerpFactoryZk is IPerpFactory, Initializable {
     // Store the addresses of account balance by index
     mapping(uint256 => address) public accountBalanceByIndex;
 
-    // Used to store the address and name of volatility at a particular _index (incremental state of 1)
-    uint256 public baseTokenIndexCount;
-
-    // Used to store the address and name of collateral at a particular _index (incremental state of 1)
-    uint256 public quoteTokenIndexCount;
-
     // Used to store the address of perp ecosystem at a particular _index (incremental state of 1)
     uint256 public perpIndexCount;
 
     // Used to store the address of vault at a particular _index (incremental state of 1)
     uint256 public vaultIndexCount;
-
-    /**
-     * @notice Intializes the Factory and stores the implementations
-     */
-    function initialize() external initializer {}
-
-    /**
-     * @notice Clones the base token - { returns base token address }
-     *
-     * @dev Generates a salt using baseTokenIndexCount, token name and token symbol
-     * @dev Clone the base token implementation with a salt make it deterministic
-     * @dev Initializes the base token
-     *
-     * @param _name is the name of base token
-     * @param _symbol is the symbol of base token
-     * @param _priceFeed is the address of referenced price oracle
-     */
-    function cloneBaseToken(
-        string memory _name,
-        string memory _symbol,
-        address _priceFeed
-    ) external returns (IVolmexBaseToken volmexBaseToken) {
-        volmexBaseToken = new VolmexBaseToken();
-        volmexBaseToken.initialize(_name, _symbol, _priceFeed, true);
-        baseTokenByIndex[baseTokenIndexCount] = volmexBaseToken;
-        emit TokenCreated(baseTokenIndexCount, address(volmexBaseToken));
-
-        baseTokenIndexCount++;
-    }
-
-    /**
-     * @notice Clones the quote token - { returns quote token address }
-     *
-     * @dev Generates a salt using quoteTokenIndexCount, token name and token symbol
-     * @dev Clone the quote token implementation with a salt make it deterministic
-     * @dev Initializes the quote token
-     *
-     * @param _name is the name of quote token
-     * @param _symbol is the symbol of quote token
-     */
-    function cloneQuoteToken(string memory _name, string memory _symbol)
-        external
-        returns (IVolmexQuoteToken volmexQuoteToken)
-    {
-        volmexQuoteToken = new VolmexQuoteToken();
-        volmexQuoteToken.initialize(_name, _symbol, false);
-        quoteTokenByIndex[quoteTokenIndexCount] = volmexQuoteToken;
-        emit TokenCreated(quoteTokenIndexCount, address(volmexQuoteToken));
-
-        quoteTokenIndexCount++;
-    }
 
     /**
      * @notice Clones the vault
