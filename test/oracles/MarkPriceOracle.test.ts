b5e11c049be72b28fc3bad61f59d859194682cc3
vijay-volmex <vijay@volmexlabs.com>
2022-08-18 17:30:25 +0530

    VOL-1091: Update test cases

--- test/oracles/MarkPriceOracle.test.ts
+++ test/oracles/MarkPriceOracle.test.ts
@@ -1,5 +1,4 @@
 import { expect } from "chai";
-import { Signer } from "ethers";
 import { ethers, upgrades } from "hardhat";
 
 describe('MarkPriceOracle', function () {
@@ -10,22 +9,33 @@ describe('MarkPriceOracle', function () {
   let factory;
   let Factory;
   let volmexBaseToken;
+  let newToken;
   let VolmexBaseToken;
   let indexPriceOracle;
   let IndexPriceOracle;
+  let MatchingEngine;
+  let matchingEngine;
+  let erc20TransferProxy;
+  let ERC20TransferProxyTest;
+  let community;
 
   this.beforeAll(async () => {
     MarkPriceOracle = await ethers.getContractFactory("MarkPriceOracle");
+    MatchingEngine = await ethers.getContractFactory("MatchingEngineTest")
     ExchangeTest = await ethers.getContractFactory("ExchangeTest");
     Factory = await ethers.getContractFactory("Factory");
     VolmexBaseToken = await ethers.getContractFactory("VolmexBaseToken");
     IndexPriceOracle = await ethers.getContractFactory("IndexPriceOracle");
+    ERC20TransferProxyTest = await ethers.getContractFactory("ERC20TransferProxyTest");
   });
 
   beforeEach(async () => {
-    const [owner] = await ethers.getSigners();
+    const [owner, account1, account2, account3, account4] = await ethers.getSigners()
 
     exchangeTest = await ExchangeTest.deploy();
+
+    erc20TransferProxy = await ERC20TransferProxyTest.deploy()
+    community = account4.address
 
     indexPriceOracle = await upgrades.deployProxy(
       IndexPriceOracle,
@@ -50,6 +60,20 @@ describe('MarkPriceOracle', function () {
       }
     );
 
+
+    newToken = await upgrades.deployProxy(
+      VolmexBaseToken,
+      [
+        "NewToken", // nameArg
+        "NTK", // symbolArg,
+        true, // isBase
+        indexPriceOracle.address, // priceFeedArg
+      ],
+      {
+        initializer: "initialize",
+      }
+    );
+
     factory = await upgrades.deployProxy(
       Factory,
       [
@@ -63,7 +87,6 @@ describe('MarkPriceOracle', function () {
     markPriceOracle = await upgrades.deployProxy(
       MarkPriceOracle,
       [
-        exchangeTest.address,
         [10000000],
         [volmexBaseToken.address],
       ],
@@ -71,6 +94,21 @@ describe('MarkPriceOracle', function () {
         initializer: "initialize",
       }
     );
+
+    matchingEngine = await upgrades.deployProxy(
+      MatchingEngine,
+      [
+        erc20TransferProxy.address, 
+        300, 
+        community, 
+        owner.address,
+        markPriceOracle.address,
+      ],
+      {
+        initializer: "__MatchingEngineTest_init",
+      },
+    );
+    await markPriceOracle.setMatchingEngine(matchingEngine.address);
 
     await exchangeTest.setMarkPriceOracle(markPriceOracle.address);
   });
@@ -80,7 +118,6 @@ describe('MarkPriceOracle', function () {
       let receipt = await upgrades.deployProxy(
         MarkPriceOracle,
         [
-          exchangeTest.address,
           [10000000],
           [volmexBaseToken.address],
         ],
@@ -91,28 +128,11 @@ describe('MarkPriceOracle', function () {
       expect(receipt.confirmations).not.equal(0);
     });
 
-    it("Should fail to deploy if exchange address is 0", async () => {
-      await expect(
-        upgrades.deployProxy(
-          MarkPriceOracle,
-          [
-            '0x0000000000000000000000000000000000000000',
-            [10000000],
-            [volmexBaseToken.address],
-          ],
-          { 
-            initializer: "initialize",
-          }
-        ),
-      ).to.be.revertedWith("MarkSMA: Not zero address");
-    });
-
     it("Should fail to deploy if length of arrays is unequal", async () => {
       await expect(
         upgrades.deployProxy(
           MarkPriceOracle,
           [
-            exchangeTest.address,
             [10000000],
             [],
           ],
@@ -128,7 +148,6 @@ describe('MarkPriceOracle', function () {
         upgrades.deployProxy(
           MarkPriceOracle,
           [
-            exchangeTest.address,
             [100000],
             [volmexBaseToken.address],
           ],
@@ -144,7 +163,6 @@ describe('MarkPriceOracle', function () {
         upgrades.deployProxy(
           MarkPriceOracle,
           [
-            exchangeTest.address,
             [10000000],
             ['0x0000000000000000000000000000000000000000'],
           ],
@@ -159,7 +177,7 @@ describe('MarkPriceOracle', function () {
   describe("Add Observation", async () => {
     it("Should add observation", async () => {
       for (let i = 0; i < 9; i++) {
-        await exchangeTest.addObservation(10000000, 0);
+        await matchingEngine.addObservation(10000000, 0);
       }
 
       const txn = await markPriceOracle.getCumulativePrice(10000000, 0);
@@ -171,15 +189,22 @@ describe('MarkPriceOracle', function () {
       expect(Number(txn)).equal(10000000);
     });
 
+    it("Should add multiple observations", async () => {
+      await matchingEngine.addAssets([10000000, 20000000], [volmexBaseToken.address, newToken.address]);
+
+      const txn = await markPriceOracle.getCumulativePrice(10000000, 0);
+      expect(Number(txn)).equal(10000000);
+    });
+
     it("Should fail to add observation when caller is not exchange", async () => {
       await expect(
         markPriceOracle.addObservation(1000000, 0)
-      ).to.be.revertedWith("MarkSMA: Not Exchange");
+      ).to.be.revertedWith("MarkSMA: Not MatchingEngine");
     });
 
     it("Should fail to add observation when cumulative price is <= 1000000", async () => {
       await expect(
-        exchangeTest.addObservation(1000000, 0)
+        matchingEngine.addObservation(1000000, 0)
       ).to.be.revertedWith("MarkSMA: Not decimal precise");
     });
 
