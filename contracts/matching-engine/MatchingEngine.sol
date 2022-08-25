388542dd235f0dfaa15dacca6b5c5d088ddf37e5
shailu-s <emailid462@gmail.com>
2022-08-25 09:38:55 +0530

    VOL-1100: Removed order validator init

--- contracts/matching-engine/MatchingEngine.sol
+++ contracts/matching-engine/MatchingEngine.sol
@@ -14,7 +14,6 @@ contract MatchingEngine is MatchingEngineCore, TransferManager {
         __Ownable_init_unchained();
         __TransferExecutor_init_unchained(erc20TransferProxy);
         __TransferManager_init_unchained();
-        __OrderValidator_init_unchained();
         __Pausable_init_unchained();
 
         _transferOwnership(owner);
