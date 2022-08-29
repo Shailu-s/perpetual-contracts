1a6e0c4f31b3d4779176bd644d3d45ddf1fd925a
vijay-volmex <vijay@volmexlabs.com>
2022-08-29 11:28:40 +0530

    VOL-1104: Add RoleManager helper contract to manage role based access

---
+++ contracts/helpers/RoleManager.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL - 1.1
+
+pragma solidity =0.8.12;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract RoleManager is AccessControl {
+    bytes32 public constant CLONES_DEPLOYER = keccak256("CLONES_DEPLOYER");
+    bytes32 public constant ONLY_ADMIN = keccak256("ONLY_ADMIN");
+    bytes32 public constant CAN_CANCEL_ALL_ORDERS = keccak256("CAN_CANCEL_ALL_ORDERS");
+    bytes32 public constant CAN_MATCH_ORDERS = keccak256("CAN_MATCH_ORDERS");
+    bytes32 public constant CAN_ADD_ASSETS = keccak256("CAN_ADD_ASSETS");
+    bytes32 public constant CAN_ADD_OBSERVATION = keccak256("CAN_ADD_OBSERVATION");
+    bytes32 public constant CAN_SETTLE_REALIZED_PNL = keccak256("CAN_SETTLE_REALIZED_PNL");
+    bytes32 public constant DEPOSIT_WITHDRAW_IN_VAULT = keccak256("DEPOSIT_WITHDRAW_IN_VAULT");
+}
+
