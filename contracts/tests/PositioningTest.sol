85c7288750e8c99083221a22de10ae9cf2625e3e
vijay-volmex <vijay@volmexlabs.com>
2022-12-15 12:44:09 +0530

    Fix vault controller before each block

--- contracts/tests/PositioningTest.sol
+++ contracts/tests/PositioningTest.sol
@@ -13,4 +13,8 @@ contract PositioningTest is Positioning {
     function getAccountValue(address trader) external view returns (int256) {
         return _getAccountValue(trader);
     }
+
+    function registerBaseToken(address trader, address token) external {
+        _registerBaseToken(trader, token);
+    }
 }
