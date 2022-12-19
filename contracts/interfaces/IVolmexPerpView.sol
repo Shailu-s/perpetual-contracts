e86e9b9e648d06b55791a782dc2d23a83a76e2d6
vijay-volmex <vijay@volmexlabs.com>
2022-12-19 10:25:45 +0530

    VOL-1337: Added perp view interface

---
+++ contracts/interfaces/IVolmexPerpView.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity =0.8.12;
+
+import "./IPositioning.sol";
+import "./IVaultController.sol";
+import "./IVolmexBaseToken.sol";
+import "./IVolmexQuoteToken.sol";
+import "./IAccountBalance.sol";
+
+interface IVolmexPerpView {
+    function perpIndexCount() external view returns (uint256);
+    function vaultIndexCount() external view returns (uint256);
+    function baseTokenIndexCount() external view returns (uint256);
+    function quoteTokenIndexCount() external view returns (uint256);
+    function positionings(uint256 _index) external view returns (IPositioning);
+    function vaultControllers(uint256 _index) external view returns (IVaultController);
+    function baseTokens(uint256 _index) external view returns (IVolmexBaseToken);
+    function quoteTokens(uint256 _index) external view returns (IVolmexQuoteToken);
+    function accounts(uint256 _index) external view returns (address);
+    function setBaseToken(IVolmexBaseToken _baseToken) external;
+    function setQuoteToken(IVolmexQuoteToken _quoteToken) external;
+    function setPositioning(IPositioning _positioning) external;
+    function setVaultController(IVaultController _vaultController) external;
+    function setAccount(IAccountBalance _account) external;
+    function incrementPerpIndex() external;
+    function incrementVaultIndex() external;
+}
