ed3c1044a6e58b1e420a15fb0bc7264af8cc0e67
shailu-s <emailid462@gmail.com>
2023-01-16 14:23:59 +0530

    VOL-1334: Update mock positioning contract

--- contracts/tests/PositioningTest.sol
+++ contracts/tests/PositioningTest.sol
@@ -9,10 +9,6 @@ contract PositioningTest is Positioning {
         makerMinSalt[_msgSender()] = _val;
     }
 
-    function getAccountValue(address trader) external view returns (int256) {
-        return _getAccountValue(trader);
-    }
-
     function registerBaseToken(address trader, address token) external {
         _registerBaseToken(trader, token);
     }
