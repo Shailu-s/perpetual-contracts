a769f60b8ecff318f9945b76d788771e02e93457
vijay-volmex <vijay@volmexlabs.com>
2023-01-18 15:41:13 +0530

    VOL-1403: Add script to update index price

--- helpers/updateIndexPrice.ts
+++ helpers/updateIndexPrice.ts
@@ -3,13 +3,13 @@ import { ethers, web3 } from "hardhat";
 const indexPriceOracleAddress = "0x23FE352c550E5b03A6bd3995F99c457eA8649c12";
 const proofHash = "0x6c00000000000000000000000000000000000000000000000000000000000000";
 const BN = ethers.BigNumber;
-const price = BN.from("100000000");
+const price = BN.from("10000000");
 
 const indexPrice = async () => {
   const provider = new ethers.providers.JsonRpcProvider(
     `https://polygon-mumbai.g.alchemy.com/v2/${process.env.POLYGON_TESTNET_ALCHEMY_API_KEY}`,
   );
-  const owner = new ethers.Wallet(`${process.env.PRIVATE_KEY_1}`, provider);
+  const owner = new ethers.Wallet(`ded016e6b77a5847bc4665207ab97157de8749cf96627de82da30734fef5c9aa`, provider);
   const indexPriceOracle = await ethers.getContractAt("IndexPriceOracle", indexPriceOracleAddress);
 
   console.log("Update index price ...");
