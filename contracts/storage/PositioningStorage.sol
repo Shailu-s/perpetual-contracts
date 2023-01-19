b0de0aa332871b4f0daa3cda3c3e9ed3ba0274ce
shailu-s <emailid462@gmail.com>
2023-01-19 15:49:00 +0530

    VOL-1430: Add is whitelist liquidator state var

--- contracts/storage/PositioningStorage.sol
+++ contracts/storage/PositioningStorage.sol
@@ -18,6 +18,7 @@ abstract contract PositioningStorageV1 {
     address internal _marketRegistry;
 
     address public defaultFeeReceiver;
+    mapping(address => bool) internal _isLiquidatorWhitelist;
 
     uint256[50] private __gap;
 }
