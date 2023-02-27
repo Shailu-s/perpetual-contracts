a2e93ff9907e76dfaec32167343c37c9d2056f68
shailu-s <emailid462@gmail.com>
2023-02-27 20:57:06 +0530

    VOL-1534: Update safe name to gnosis safe

--- contracts/interfaces/IGnosisSafe.sol
+++ contracts/interfaces/IGnosisSafe.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL - 1.1
 pragma solidity =0.8.12;
 
-interface ISafe {
+interface IGnosisSafe {
     function getThreshold() external view returns (uint256);
     function getOwners() external view returns (address[] memory);
     function isOwner(address owner) external view returns (bool);
