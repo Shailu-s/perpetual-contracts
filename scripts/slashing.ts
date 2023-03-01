4c91d0604f5942821c64274314f92c86bdffe804
shailu-s <emailid462@gmail.com>
2023-03-01 19:46:27 +0530

    Add custom fee for chainid == 5

--- scripts/slashing.ts
+++ scripts/slashing.ts
@@ -1,6 +1,7 @@
 import { ethers, upgrades, run } from "hardhat";
 
 const slashing = async () => {
+  const { chainId } = await ethers.provider.getNetwork();
   const [owner] = await ethers.getSigners();
   console.log("Deployer: ", await owner.getAddress());
   console.log("Balance: ", (await owner.getBalance()).toString());
@@ -28,24 +29,36 @@ const slashing = async () => {
     insuraceAddress = insurance.address;
     console.log("Insurance: ", insuraceAddress);
   }
+
+  if (chainId == 5) {
+    const feeData = await ethers.provider.getFeeData();
+    const signer = ethers.provider.getSigner();
+    signer.estimateGas = async transaction => {
+      return feeData.maxFeePerGas;
+    };
+  }
   const relayerSafe = process.env.RELAYER_MULTISIG;
-  const volmexSafe = process.env.VOLMEX_MULTISIG || await owner.getAddress();
-  const slashing = await upgrades.deployProxy(Slashing, [
-    usdcAddress,
-    relayerSafe,
-    volmexSafe,
-    volmexSafe,
-    300, // 5 days, should be
-    insuraceAddress,
-  ], {
-    initializer: "Slashing_init"
-  });
+  const volmexSafe = process.env.VOLMEX_MULTISIG || (await owner.getAddress());
+  const slashing = await upgrades.deployProxy(
+    Slashing,
+    [
+      usdcAddress,
+      relayerSafe,
+      volmexSafe,
+      volmexSafe,
+      300, // 5 days, should be
+      insuraceAddress,
+    ],
+    {
+      initializer: "Slashing_init",
+    },
+  );
   await slashing.deployed();
   await verifyTask(await proxyAdmin.getProxyImplementation(slashing.address));
   console.log("Slashing: ", slashing.address);
 
   await (await slashing.toggleStaking()).wait();
-  console.log("DONE !!!")
+  console.log("DONE !!!");
 };
 
 slashing()
@@ -63,4 +76,4 @@ const verifyTask = async (contract: string) => {
   } catch (error) {
     console.log("ERROR - verify");
   }
-}
+};
