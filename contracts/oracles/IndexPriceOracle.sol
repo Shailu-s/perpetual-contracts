612f2b86a8f1711071ab9ca5a9267aa4cde6d3b4
shailu-s <emailid462@gmail.com>
2023-03-27 11:36:04 +0530

    VOL-1626: Update arg types

--- contracts/oracles/IndexPriceOracle.sol
+++ contracts/oracles/IndexPriceOracle.sol
@@ -11,15 +11,15 @@ import "./BaseOracle.sol";
  * @title Volmex Oracle contract
  * @author volmex.finance [security@volmexlabs.com]
  */
-contract IndexPriceOracle is BaseOracle, ERC165StorageUpgradeable, IIndexPriceOracle {
+contract IndexPriceOracle is BaseOracle, ERC165StorageUpgradeable {
     // Interface ID of VolmexOracle contract, hashId = 0xf9fffc9f
     bytes4 private constant _IVOLMEX_ORACLE_ID = type(IIndexPriceOracle).interfaceId;
 
     /**
      * @notice Initializes the contract setting the deployer as the initial owner.
      */
-    function initialize(address _admin, address[2] calldata _volatilityIndex, bytes32[2] calldata _proofHash, uint256[2] calldata _capRatio,) external initializer {
-        _BaseOracle_init([400000000, 400000000], _volatilityIndex, _proofHash, _capRatio, _admin);
+    function initialize(address _admin, uint256[] calldata _volatilityPrices, address[] calldata _volatilityIndex, bytes32[] calldata _proofHash, uint256[] calldata _capRatio) external initializer {
+        _BaseOracle_init(_volatilityPrices, _volatilityIndex, _proofHash, _capRatio, _admin);
 
         __ERC165Storage_init();
         _registerInterface(_IVOLMEX_ORACLE_ID);
@@ -31,7 +31,7 @@ contract IndexPriceOracle is BaseOracle, ERC165StorageUpgradeable, IIndexPriceOr
      * @param _twInterval time for averaging observations
      * @return answer is the answer for the given round
      */
-    function latestRoundData(uint256 _twInterval, uint256 _index) external view virtual returns (uint256 answer, uint256 lastUpdateTimestamp) {
+    function latestRoundData(uint256 _twInterval, uint64 _index) external view virtual returns (uint256 answer, uint256 lastUpdateTimestamp) {
         (answer, lastUpdateTimestamp) = _getCumulativePrice(_twInterval, _index);
         answer *= 100;
     }
