ce65d43e190249c970307ae059dae304647ea981
shailu-s <emailid462@gmail.com>
2023-04-17 12:13:37 +0530

    VOL-1890: added library test for safe cast

---
+++ contracts/tests/libs/LibSafeCastTest.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.18;
+pragma abicoder v2;
+
+import { LibSafeCastInt } from "../../libs/LibSafeCastInt.sol";
+import { LibSafeCastUint } from "../../libs/LibSafeCastUint.sol";
+
+contract TestPerpSafeCast {
+    using LibSafeCastUint for int256;
+    using LibSafeCastInt for uint256;
+
+    // uint test
+
+    function testToUint128(int256 value) external pure returns (int128) {
+        return value.toInt128();
+    }
+
+    function testToUint64(uint256 value) external pure returns (uint64) {
+        return value.toUint64();
+    }
+
+    function testToUint32(uint256 value) external pure returns (uint32) {
+        return value.toUint32();
+    }
+
+    function testToUint16(uint256 value) external pure returns (uint16) {
+        return value.toUint16();
+    }
+
+    function testToUint8(uint256 value) external pure returns (uint8) {
+        return value.toUint8();
+    }
+
+    // int test
+
+    function testToInt128(int256 value) external pure returns (int128) {
+        return value.toInt128();
+    }
+
+    function testToInt64(int256 value) external pure returns (int64) {
+        return value.toInt64();
+    }
+
+    function testToInt32(int256 value) external pure returns (int32) {
+        return value.toInt32();
+    }
+
+    function testToInt24(int256 value) external pure returns (int24) {
+        return value.toInt24();
+    }
+
+    function testToInt16(int256 value) external pure returns (int16) {
+        return value.toInt16();
+    }
+
+    function testToInt8(int256 value) external pure returns (int8) {
+        return value.toInt8();
+    }
+}
--- test/libs/LibSettlementTokenMath.test.ts
+++ test/libs/LibSettlementTokenMath.test.ts
@@ -3,11 +3,7 @@ import { BigNumber } from "ethers";
 import { parseEther, parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
-describe.only("SettlementTokenMath test", async () => {
-  const maxUint256 = BigNumber.from(2).pow(256).sub(1);
-  const maxInt256 = BigNumber.from(2).pow(255).sub(1);
-  const minInt256 = BigNumber.from(2).pow(255).mul(-1);
-
+describe("SettlementTokenMath test", async () => {
   let settlementTokenMath: any;
 
   beforeEach(async () => {
@@ -16,60 +12,42 @@ describe.only("SettlementTokenMath test", async () => {
   });
 
   describe("parseSettlementToken", async () => {
-    it("parse uint256 from 8 decimals", async () => {
+    it("parse uint256 from 18 decimals", async () => {
       expect(
         await settlementTokenMath["testParseSettlementToken(uint256,uint8)"](
-          parseUnits("1000", 8),
-          8,
+          parseUnits("1000", 18),
+          18,
         ),
       ).to.be.eq(parseEther("1000"));
     });
 
-    it("parse int256 from 8 decimals", async () => {
+    it("parse int256 from 18 decimals", async () => {
       expect(
         await settlementTokenMath["testParseSettlementToken(int256,uint8)"](
-          parseUnits("-1000", 8),
-          8,
+          parseUnits("-1000", 18),
+          18,
         ),
       ).to.be.eq(parseEther("-1000"));
     });
   });
 
   describe("formatSettlementToken", async () => {
-    it("format uint256 to 8 decimals", async () => {
+    it("format uint256 to 18 decimals", async () => {
       expect(
         await settlementTokenMath["testFormatSettlementToken(uint256,uint8)"](
           parseEther("1000"),
-          8,
+          18,
         ),
-      ).to.be.eq(parseUnits("1000", 8));
+      ).to.be.eq(parseUnits("1000", 18));
     });
 
-    it("format int256 to 8 decimals", async () => {
+    it("format int256 to 18 decimals", async () => {
       expect(
         await settlementTokenMath["testFormatSettlementToken(int256,uint8)"](
           parseEther("-1000"),
-          8,
+          18,
         ),
-      ).to.be.eq(parseUnits("-1000", 8));
-    });
-
-    it("format with rounding down on positive number", async () => {
-      expect(
-        await settlementTokenMath["testFormatSettlementToken(int256,uint8)"](
-          "100123456789123456789",
-          8,
-        ),
-      ).to.be.eq("10012345678");
-    });
-
-    it("format with rounding down on negative number", async () => {
-      expect(
-        await settlementTokenMath["testFormatSettlementToken(int256,uint8)"](
-          "-100123456789123456789",
-          8,
-        ),
-      ).to.be.eq("-10012345678");
+      ).to.be.eq(parseUnits("-1000", 18));
     });
   });
 });
