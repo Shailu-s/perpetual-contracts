28a78f1a1e95d6596e465dfb1986829bf4fac871
shailu-s <emailid462@gmail.com>
2023-04-26 16:46:29 +0530

    VOL-1912: Removed parent token and update its use

--- contracts/tokens/VolmexBaseToken.sol
+++ contracts/tokens/VolmexBaseToken.sol
@@ -2,11 +2,35 @@
 pragma solidity =0.8.18;
 
 import { IIndexPriceOracle } from "../interfaces/IIndexPriceOracle.sol";
-import { ParentToken } from "./base/ParentToken.sol";
+import { IVolmexBaseToken } from "../interfaces/IVolmexBaseToken.sol";
+import { VirtualToken } from "./VirtualToken.sol";
+import { BaseTokenStorageV1 } from "../storage/BaseTokenStorage.sol";
 
 // never inherit any new stateful contract. never change the orders of parent stateful contracts
-contract VolmexBaseToken is ParentToken {
-    function getIndexPrice(uint256 index) external view override returns (uint256 answer) {
+abstract contract VolmexBaseToken is IVolmexBaseToken, VirtualToken, BaseTokenStorageV1 {
+    function initialize(
+        string memory nameArg,
+        string memory symbolArg,
+        address priceFeedArg,
+        bool isBase
+    ) external override initializer {
+        __VirtualToken_init(nameArg, symbolArg, isBase);
+        _priceFeed = priceFeedArg;
+    }
+
+    /// @dev This function is only used for emergency shutdown, to set priceFeed to an emergencyPriceFeed
+    function setPriceFeed(address priceFeedArg) external virtual {
+        _priceFeed = priceFeedArg;
+        emit PriceFeedChanged(_priceFeed);
+    }
+
+    /// @dev Get price of underlying asset with sma interval
+    function getIndexPrice(uint256 index, uint256 _twInterval) external view returns (uint256 answer) {
         (answer, ) = IIndexPriceOracle(_priceFeed).latestRoundData(_twInterval, index);
+    }
+
+    /// @inheritdoc IVolmexBaseToken
+    function getPriceFeed() external view override returns (address) {
+        return _priceFeed;
     }
 }
