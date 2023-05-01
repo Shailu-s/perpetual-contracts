1906fa6e4959a355178e07cf5ee2c01d87893976
shailu-s <emailid462@gmail.com>
2023-05-01 21:33:25 +0530

    VOL-1975: Init oracles with initial values and setup

--- contracts/oracles/PerpetualOracles.sol
+++ contracts/oracles/PerpetualOracles.sol
@@ -20,7 +20,8 @@ contract PerpetualOracles is AccessControlUpgradeable {
     }
 
     bytes32 public constant PRICE_ORACLE_ADMIN = keccak256("PRICE_ORACLE_ADMIN");
-    bytes32 public constant ADD_OBSERVATION_ROLE = keccak256("ADD_OBSERVATION_ROLE");
+    bytes32 public constant ADD_MARK_OBSERVATION_ROLE = keccak256("ADD_MARK_OBSERVATION_ROLE");
+    bytes32 public constant ADD_INDEX_OBSERVATION_ROLE = keccak256("ADD_INDEX_OBSERVATION_ROLE");
     bytes32 public constant INITIAL_TIMESTAMP_ROLE = keccak256("INITIAL_TIMESTAMP_ROLE");
 
     uint256 internal _indexCount;
@@ -37,17 +38,52 @@ contract PerpetualOracles is AccessControlUpgradeable {
     event ObservationAdderSet(address indexed matchingEngine);
     event ObservationAdded(uint256[] index, uint256[] underlyingPrice, uint256 timestamp);
 
-    function perpetualOraclesInit() external initializer {}
-
-    function _requireOracleAdmin() internal view {
-        require(hasRole(PRICE_ORACLE_ADMIN, _msgSender()), "IndexPriceOracle: not admin");
+    function perpetualOraclesInit(
+        address[2] calldata _baseToken,
+        uint256[2] calldata _markPrices,
+        uint256[2] calldata _indexPrices,
+        bytes32[2] calldata _proofHashes,
+        address _admin
+    ) external initializer {
+        uint256 indexCount;
+        for (; indexCount < 2; ++indexCount) {
+            baseTokenByIndex[indexCount] = _baseToken[indexCount];
+            indexByBaseToken[_baseToken[indexCount]] = indexCount;
+            markObservations[indexCount].push(MarkObservation({timestamp: block.timestamp, lastPrice: _markPrices[indexCount]}));
+            indexObservations[indexCount].push(IndexObservation({timestamp: block.timestamp, underlyingPrice: _indexPrices[indexCount], proofHash: _proofHashes[indexCount]}));
+        }
+        _indexCount = indexCount; // = 2
+        _grantRole(PRICE_ORACLE_ADMIN, _admin);
+        _setRoleAdmin(PRICE_ORACLE_ADMIN, PRICE_ORACLE_ADMIN);
     }
 
-    function _requireCanAddObservation() internal view {
-        require(hasRole(ADD_OBSERVATION_ROLE, _msgSender()), "IndexPriceOracle: not observation adder");
+    function setMarkObservationAdder(address _adder) external {
+        _requireOracleAdmin();
+        require(_adder != address(0), "PerpOracle: zero address");
+        _grantRole(ADD_MARK_OBSERVATION_ROLE, _adder);
+        emit ObservationAdderSet(_adder);
+    }
+
+    function setIndexObservationAdder(address _adder) external {
+        _requireOracleAdmin();
+        require(_adder != address(0), "PerpOracle: zero address");
+        _grantRole(ADD_INDEX_OBSERVATION_ROLE, _adder);
+        emit ObservationAdderSet(_adder);
+    }
+
+    function _requireOracleAdmin() internal view {
+        require(hasRole(PRICE_ORACLE_ADMIN, _msgSender()), "PerpOracle: not admin");
+    }
+
+    function _requireAddMarkObservationRole() internal view {
+        require(hasRole(ADD_MARK_OBSERVATION_ROLE, _msgSender()), "PerpOracle: not mark observation adder");
+    }
+
+    function _requireAddIndexObservationRole() internal view {
+        require(hasRole(ADD_INDEX_OBSERVATION_ROLE, _msgSender()), "PerpOracle: not index observation adder");
     }
 
     function _requireInitialTimestampRole() internal view {
-        require(hasRole(INITIAL_TIMESTAMP_ROLE, _msgSender()), "IndexPriceOracle: not first interval adder");
+        require(hasRole(INITIAL_TIMESTAMP_ROLE, _msgSender()), "PerpOracle: not first interval adder");
     }
 }
