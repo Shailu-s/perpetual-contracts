8744c9c62d973b23a0416a213ac384669e7471d5
shailu-s <emailid462@gmail.com>
2023-05-18 20:49:55 +0530

    VOL-2006: removed .only

--- test/periphery/VolmexPerpPeriphery.test.ts
+++ test/periphery/VolmexPerpPeriphery.test.ts
@@ -6,7 +6,7 @@ const { Order, Asset, sign, encodeAddress } = require("../order");
 import { BigNumber } from "ethers";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 
-describe.only("VolmexPerpPeriphery", function () {
+describe("VolmexPerpPeriphery", function () {
   let MatchingEngine;
   let matchingEngine;
   let VirtualToken;
