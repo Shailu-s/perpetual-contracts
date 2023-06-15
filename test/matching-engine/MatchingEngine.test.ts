94b721ac43baf26e6c159a19d8ce8907535cae7f
shailu-s <emailid462@gmail.com>
2023-06-15 17:34:56 +0530

    VOL-2189: Add fill test for right with taker

--- test/matching-engine/MatchingEngine.test.ts
+++ test/matching-engine/MatchingEngine.test.ts
@@ -2112,6 +2112,34 @@ describe("MatchingEngine", function () {
       expect(newFills.leftValue).equal(convert(10));
       expect(newFills.rightValue).equal(convert(100));
     });
+
+    it("Should left partial and right complete fill", async () => {
+      orderLeft = Order(
+        ORDER,
+        deadline,
+        account1.address,
+        Asset(virtualToken.address, convert(220)),
+        Asset(volmexBaseToken.address, convert(20)),
+        ++salt,
+        0,
+        !isShort,
+      );
+
+      orderRight = Order(
+        ORDER,
+        deadline,
+        account2.address,
+        Asset(volmexBaseToken.address, convert(10)),
+        Asset(virtualToken.address, convert(100)),
+        ++salt,
+        0,
+        isShort,
+      );
+      const receipt = await (await matchingEngine.matchOrders(orderLeft, orderRight)).wait();
+      const newFills = matchedFills(receipt);
+      expect(newFills.leftValue).equal(convert(110));
+      expect(newFills.rightValue).equal(convert(10));
+    });
   });
 
   describe("Max order size - fills", () => {
