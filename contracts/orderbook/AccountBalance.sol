02a83d073417342e022a7d832b9d6a30a03986b9
shailu-s <emailid462@gmail.com>
2023-06-19 15:29:33 +0530

    VOL-2218: Add emit event for zero time to wait

--- contracts/orderbook/AccountBalance.sol
+++ contracts/orderbook/AccountBalance.sol
@@ -363,6 +363,7 @@ contract AccountBalance is IAccountBalance, BlockContext, PositioningCallee, Acc
         if (timeToWait > 0) require(nextLiquidationTime[trader] <= block.timestamp, "AB_ELT"); // early liquidation triggered
         nextLiquidationTime[trader] = block.timestamp + timeToWait;
         if (accountValue < 0) emit TraderBadDebt(trader, accountValue);
+        if (timeToWait == 0) emit ZeroMaxTimeBound(trader, timeToWait);
         emit TraderNextLiquidationUpdated(trader, baseToken, nextLiquidationTime[trader]);
     }
 
