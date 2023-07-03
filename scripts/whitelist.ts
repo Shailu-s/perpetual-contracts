4b1db8ec57631dfd01d7201119c613a5e33f80d1
jonomnom <jonomnom5097@gmail.com>
2023-07-03 02:01:16 -0700

    update whitelist script

--- scripts/whitelist.ts
+++ scripts/whitelist.ts
@@ -6,37 +6,37 @@ import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
 const contracts = {
-  PERPETUAL_ORACLE: "0x26AC26788bd7fdEd6B1AB70629214C2Aa211d8c9",
-  BASE_TOKEN: "0x6153C83845fcd2E406a4732de30fC06b1031FaC7",
-  BASE_TOKEN_2: "0xDF3A92A7B883B482651388eC5b71370A3A038Db3",
-  USDC: "0xF398bDAF7044Bb745188B4E8827D57c602C4D9e4",
-  USDT: "0xF398bDAF7044Bb745188B4E8827D57c602C4D9e4",
-  MATCHING_ENGINE: "0x08c37AC1a265e156f4f88C0f06eBfc258D134B47",
-  POSITIONING_CONFIG: "0x49dE9C3E84afC77e69438B43F9721F3c9D6bBAc2",
-  ACCOUNT_BALANCE: "0x9b8d2191963c992e4F8F900fa1cC60ce5366161B",
-  VAULT: "0xb8949d29964591792a05F2906aeBdEAA1a912612",
-  VAULT_CONTROLLER: "0xeA4aBD487aB013C95F2530922aeA007324b83240",
-  POSITIONING: "0xD83d5bfF0B20D7eB34125B86879Cbd2Fc047a741",
-  PERIPHERY: "0x4C333EE98201D1062E9f7c2404a6c48fD7B67D87",
-  MARKET_REGISTRY: "0x9DD0f5797fDf18B534a7Ac85fE2f3cd8E0d2E688",
-  QUOTE_TOKEN: "0xfe0B36Cee1B08D160b5031A3b397b61A261DC333",
-  Perp_View: "0x4b9B35648Cd2A70f530683C712fCc40d245e860B",
-  PERP_FACTORY: "0xf2726eaD68ab70b13Bf79303017e29581cafE5c3",
-};
+  PERPETUAL_ORACLE: '0x424EE289F0Fb5c22DcB842f98e9FeCEF8bB34cFa',
+  BASE_TOKEN: '0x0218fB8A664d0E15A34cBfa5E606ae5ED66cFb37',
+  BASE_TOKEN_2: '0xDd1A614AD591d7A17953Bb4314c630ae2865D5B7',
+  USDC: '0xB3011837c08D3A447AC1e08CCBAb30caBFC50511',
+  USDT: '0xB3011837c08D3A447AC1e08CCBAb30caBFC50511',
+  MATCHING_ENGINE: '0x78e88Ecbd262439ea27B5D5c480f74f576E123C7',
+  POSITIONING_CONFIG: '0x2EF3c3CbF567bb90129B8799A378d56Afc8bA8E4',
+  ACCOUNT_BALANCE: '0x38350A699760F29360f47580B9158c87f8F43d67',
+  VAULT: '0x8b2789C0C6163b120E6A3949FB528cBbBAA6879A',
+  VAULT_CONTROLLER: '0x3a8672BC3FFC98A2b889f3B1B6c0dAc446b3ef85',
+  POSITIONING: '0x2B1A5168fC68a6ebd7ff00Be9d9764E00C021AEe',
+  PERIPHERY: '0x4c7789A5E943d6Be38E60d2d96F2E47bf491574a',
+  MARKET_REGISTRY: '0xD30F628D389a1522606a430BDfEA7Ea30E5b947D',
+  QUOTE_TOKEN: '0x78F103d2AF2594E178D22A46EF55d8ccc5c6a159',
+  Perp_View: '0xdb99900320EAE281630F04ad29637AF2501f026d',
+  PERP_FACTORY: '0x74bC67ed6948f0a4C387C353975F142Dc640537a',
+}
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
-    "https://icy-fabled-meadow.base-goerli.quiknode.pro/5552ae4af262e5dca077a7a9c26b3cb484599925/",
+    "https://arbitrum-goerli.infura.io/v3/e2a0f0db1ef34ac69c77250c55f0b6fd",
   );
   const account0 = new ethers.Wallet( // deployer account
     process.env.DEPLOYER_PRIVATE_KEY,
     provider,
   );
 
-  const traderWhitelister = new ethers.Wallet(
-    process.env.TRADER_WHITELISTER_PRIVATE_KEY,
-    provider,
-  );
+  // const traderWhitelister = new ethers.Wallet(
+  //   process.env.TRADER_WHITELISTER_PRIVATE_KEY,
+  //   provider,
+  // );
 
   //   const account2 = new ethers.Wallet(
   //     "b2eada77569027933f70e63fbb356a1193bcb3c3d52cba3859d5fd73103d69c8",
@@ -63,16 +63,13 @@ async function main() {
   //     .connect(account0)
   //     .whitelistTrader(aditya1Address, true, { gasLimit: 20000000, gasPrice: 1600000000 });
 
-  const gasLimit = await perpPeriphery
-    .connect(traderWhitelister)
-    .estimateGas.whitelistTrader(testAccount1Address, true);
-  //   return;
+
   const res1 = await perpPeriphery
-    .connect(traderWhitelister)
-    .whitelistTrader(testAccount1Address, true, { gasLimit: gasLimit, gasPrice: 2000000000 });
+    .connect(account0)
+    .whitelistTrader('0x0c85B265898E81327CD9227Fad955D28a439B55A', true);
   const f = await res1.wait();
   console.log("f", f.transactionHash);
-
+    return;
   function convert(num) {
     const one = BigNumber.from(ethers.constants.WeiPerEther.toString()); // 1e18 in string
     return BigNumber.from(num).mul(one).toString();
