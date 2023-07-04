f6295231f972ddfd8db2b242044965d062988c5a
shailu-s <emailid462@gmail.com>
2023-07-04 14:28:19 +0530

    VOL-2267: Add vars for funding rate

--- contracts/storage/FundingRateStorage.sol
+++ contracts/storage/FundingRateStorage.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: BUSL - 1.1
 pragma solidity =0.8.18;
 
+import { IAccountBalance } from "../interfaces/IAccountBalance.sol";
+import { IPerpetualOracle } from "../interfaces/IPerpetualOracle.sol";
+import { IPositioningConfig } from "../interfaces/IPositioningConfig.sol";
+
 /// @notice For future upgrades, do not change ExchangeStorageV1. Create a new
 /// contract which implements ExchangeStorageV1 and following the naming convention
 /// ExchangeStorageVX.
 abstract contract FundingRateStorage {
     int256 internal constant _PRECISION_BASE = 1e18;
     int256 internal constant _IORACLE_BASE = 1e6;
-    address internal _perpetualOracleArg;
     uint256 internal _fundingPeriod;
+    IPerpetualOracle internal _perpetualOracleArg;
+    IPositioningConfig public positioningConfig;
+    IAccountBalance public accountBalance;
     mapping(address => uint256) internal _lastFundingIndexPrice;
     mapping(address => int256) internal _lastFundingRate;
-    mapping(address => uint256) internal _underlyingPriceIndexes;
+    mapping(address => uint256) internal _lastSettledTimestampMap; // the last timestamp when funding is settled
+    mapping(address => int256) internal _globalFundingGrowthMap; // base token => twPremium
+    mapping(address => uint256) internal _firstTradedTimestampMap;
 
     uint256[50] private __gap;
 }
