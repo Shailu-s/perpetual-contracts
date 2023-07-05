a7a2c85fa99d4652e558bddda9f9c2a79c947383
shailu-s <emailid462@gmail.com>
2023-07-05 20:12:55 +0530

    VOL-2265: updated black listing addresses

--- helpers/blacklistAccounts.ts
+++ helpers/blacklistAccounts.ts
@@ -1,14 +1,14 @@
 import { ethers, web3 } from "hardhat";
-import { blacklisedAddresses } from "../utils/blacklistedAddresses";
+import { blacklistedAddresses } from "../utils/blacklistedAddresses";
 const blacklistAccounts = async () => {
   const Periphery = await ethers.getContractFactory("VolmexPerpPeriphery");
   const periphery = Periphery.attach(`${process.env.PERIPHERY_ADDRESS}`);
-  const iterations = Number((blacklisedAddresses.length / 50).toFixed(0));
-  const isBlacklist = new Array(blacklisedAddresses.length).fill(true);
+  const iterations = Number((blacklistedAddresses.length / 50).toFixed(0));
+  const isBlacklist = new Array(blacklistedAddresses.length).fill(true);
   for (let index = 0; index < iterations; index++) {
     periphery.blacklistAccounts(
-      blacklisedAddresses.slice(50 * index, 50 * (index + 1)),
-      isBlacklist.slice(50 * index, 50 * (index + 1)),
+      blacklistedAddresses.slice(200 * index, 200 * (index + 1)),
+      isBlacklist.slice(200 * index, 200 * (index + 1)),
     );
   }
 };
