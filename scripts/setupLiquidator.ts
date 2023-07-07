817bfa1df77fc4b5dc140bca88327feb75a38370
jonomnom <jonomnom5097@gmail.com>
2023-07-06 18:34:08 -0700

    updated setupLiquidator script to deposit and approve

--- scripts/setupLiquidator.ts
+++ scripts/setupLiquidator.ts
@@ -2,22 +2,32 @@ import { ethers } from "hardhat";
 import { BigNumber, Wallet } from "ethers";
 import { constants } from "./chains";
 
+// yarn hardhat run scripts/setupLiquidator.ts --network base-goerli --no-compile
 // liquidator needs TRADER_WHITELISTER and liquidator role and some stable coins deposited into vault and eth
 
-const STABLE_COINS_AMOUNT = ethers.utils.parseUnits("1000000", 6);
-
-// yarn hardhat run scripts/setupLiquidator.ts --network base-goerli --no-compile
+const GIVE_USDT = true; // this is for testnet purposes only, on mainnet, liquidators will need to manually deposit on UI or we need to provide a script
+const STABLE_COINS_AMOUNT = ethers.utils.parseUnits("500000", 6);
+const MINIMUM_ETH_BALANCE = ethers.utils.parseUnits("0.1", 18);
 const contracts = constants["arbitrum-goerli"];
 const rpcUrl = contracts.RPC_URL;
 
 const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-const liquidatorAddress = "0xc9132B6Ac716a84CB6705AA27eE05Ed7be7b97D7"
-const account0 = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY, provider); // deployer account
-let tx
+const liquidatorAddress = "0xc9132B6Ac716a84CB6705AA27eE05Ed7be7b97D7";
+const liquidatorPrivateKey = process.env.LIQUIDATOR_PRIVATE_KEY; // ONLY FOR TESTNET PURPOSES
+const liquidator = new ethers.Wallet(liquidatorPrivateKey, provider);
+
+const deployer = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY, provider); // deployer account
+const usdGiver = new ethers.Wallet(process.env.USD_GIVER_PRIVATE_KEY, provider);
+let tx;
 async function setupLiquidator() {
+
+  if (liquidatorPrivateKey !== undefined && liquidatorAddress !== liquidator.address) {
+    throw 'liquidator address and liquidator private key do not match';
+  }
+
   const VirtualToken = await ethers.getContractFactory("VirtualToken");
   const virtualToken = await VirtualToken.attach(contracts.USDC);
-  
+
   const VolmexPerpPeriphery = await ethers.getContractFactory("VolmexPerpPeriphery");
   const perpPeriphery = await VolmexPerpPeriphery.attach(contracts.PERIPHERY);
 
@@ -27,33 +37,59 @@ async function setupLiquidator() {
   const isTraderWhitelisted = await perpPeriphery.isTraderWhitelisted(liquidatorAddress);
 
   if (isTraderWhitelisted) {
-    console.log('already whitelisted')
+    console.log("already whitelisted");
   } else {
-    tx = await perpPeriphery
-    .connect(account0)
-    .whitelistTrader(liquidatorAddress, true, { gasLimit: 20000000, gasPrice: 1600000000 });
-    tx.wait()
-    console.log("whitelisted trader", tx.hash)
+    tx = await perpPeriphery.connect(deployer).whitelistTrader(liquidatorAddress, true);
+    tx.wait();
+    console.log("whitelisted trader", tx.hash);
   }
 
-  const isLiquidator = await positioning.isLiquidatorWhitelisted(liquidatorAddress)
+  const isLiquidator = await positioning.isLiquidatorWhitelisted(liquidatorAddress);
 
   if (isLiquidator) {
-    console.log('already liquidator')
+    console.log("already liquidator");
   } else {
-    tx = await positioning
-    .connect(account0)
-    .whitelistLiquidator(liquidatorAddress, true, { gasLimit: 20000000, gasPrice: 1600000000 });
-    tx.wait()
-    console.log("whitelisted liquidator", tx.hash)
+    tx = await positioning.connect(deployer).whitelistLiquidator(liquidatorAddress, true);
+    tx.wait();
+    console.log("whitelisted liquidator", tx.hash);
   }
 
-  // TODO: add stable coins to liquidator and deposit
-  //transfer
-  //approve
-  //depositToVault
+  if (GIVE_USDT) {
+    const liquidatorEthBalance = await provider.getBalance(liquidatorAddress);
+    if (liquidatorEthBalance.gte(MINIMUM_ETH_BALANCE)) {
+      console.log("ETH balance:", ethers.utils.formatEther(liquidatorEthBalance.toString()));
+    } else {
+      tx = await deployer.sendTransaction({
+        to: liquidatorAddress,
+        value: MINIMUM_ETH_BALANCE.sub(liquidatorEthBalance),
+      });
+      tx.wait();
+      console.log("sent eth", tx.hash);
+    }
 
 
+    // nice to have: check if liquidator already has a balance so can skip these steps below
+    // convert to 18 decimals
+    //transfer
+    tx = await virtualToken.connect(usdGiver).transfer(liquidatorAddress, STABLE_COINS_AMOUNT);
+    tx.wait();
+    console.log("sent stable coins", tx.hash)
+    //approve
+    // TODO handle edge case of USDT incorrectly approved previously (need to revoke approval first)
+    tx = await virtualToken
+    .connect(liquidator)
+    .approve(contracts.PERIPHERY, STABLE_COINS_AMOUNT);
+    tx.wait();
+    console.log("approve stable coins to periphery", tx.hash)
+    //depositToVault
+    tx = await perpPeriphery
+    .connect(liquidator)
+      .depositToVault(0, contracts.USDC, STABLE_COINS_AMOUNT);
+    tx.wait();
+    console.log("deposited stable coins", tx.hash);
+
+    console.log('\n')
+  }
 }
 
 setupLiquidator()
