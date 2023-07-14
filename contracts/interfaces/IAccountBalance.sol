71230ec642829bfdda504762eec856c6e6dc0c3f
shailu-s <emailid462@gmail.com>
2023-07-14 18:41:59 +0530

    added function to get initial free collateral

--- contracts/interfaces/IAccountBalance.sol
+++ contracts/interfaces/IAccountBalance.sol
@@ -159,4 +159,7 @@ interface IAccountBalance {
     function isAccountLiquidatable(address trader, address baseToken, uint256 minOrderSize, int256 accountValue, int256 freeCollateralByRatio) external view returns (bool isLiquidatable);
     function checkAndUpdateLiquidationTimeToWait(address trader, address baseToken, int256 accountValue, uint256 minOrderSize, int256 freeCollateralByRatio) external;
     function getNLiquidate(uint256 liquidatablePositionSize, uint256 minOrderSize, uint256 maxOrderSize) external view returns (uint256 nLiquidate);
+    function getInitialPositionValue(int256 positionSize, address baseToken) external view returns (int256);
+    function getInitialDebtValue(int256 positionSize, int256 openNotional, address baseToken) external view returns(uint256);
+
 }
